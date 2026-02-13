57a6bac31a5a4040029daa8c667b80a63146ce19
shreemathi-1 <shreemathi005@gmail.com>
2026-02-13 15:17:30 +0530

    Create 50-PowxN

---
+++ 50-PowxN/50-PowxN.java
@@ -0,0 +1,21 @@
+// Last updated: 2/13/2026, 3:17:29 PM
+class Solution {
+    public double myPow(double x, long n) {
+        if (n < 0) {
+            x = 1 / x;
+            n = -n;
+        }
+
+        double result = 1;
+        double current_product = x;
+
+        while (n > 0) {
+            if (n % 2 == 1)
+                result = result * current_product;
+            current_product = current_product * current_product;
+            n = n / 2;
+        }
+
+        return result;
+    }
+}
