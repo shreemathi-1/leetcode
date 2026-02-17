c6da3f71629e1f072661bcec7f8eb6e769999f1b
shreemathi-1 <shreemathi005@gmail.com>
2026-02-17 19:18:13 +0530

    File updated at 2/17/2026, 7:18:13 PM

--- 125-ValidPalindrome/125-ValidPalindrome.java
+++ 125-ValidPalindrome/125-ValidPalindrome.java
@@ -1,33 +1,22 @@
-// Last updated: 2/13/2026, 3:17:05 PM
-class Solution 
-{
-    public boolean isPalindrome(String s) 
-    {
-        String t = "";      
-        s = s.toLowerCase();
-        for(int i =0;i<s.length();i++)
-        {
-            if(Character.isLetterOrDigit(s.charAt(i) ) )
-            {
-                t += s.charAt(i);
-            }
-        }
-        int n = t.length();
-        int l =0,r = n -1;
-            while(l < r)
-            {
-                if(t.charAt(l) != t.charAt(r) )
-                {
-                     return false;
-                   
-                }
-                else
-                {
-                     l++;
-                    r--;
-                }
-                
-            }
-  return true;
-    }
-}
+// Last updated: 2/17/2026, 7:18:13 PM
+1class Solution 
+2{
+3    public boolean isPalindrome(String s)
+4     {
+5        StringBuilder sb = new StringBuilder();
+6        s = s.toLowerCase() ;
+7        for(int i = 0;i < s.length();i++ )
+8        {
+9            if(Character.isLetterOrDigit(s.charAt(i)) )
+10            {
+11                sb.append(s.charAt(i) );
+12            }
+13        }
+14       
+15        if(sb.toString().equals( sb.reverse().toString()) )
+16        {
+17            return true;
+18        }
+19        return false;
+20    }
+21}
