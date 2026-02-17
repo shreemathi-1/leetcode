be640932a97880c884eea9414bea0006abbc47fd
shreemathi-1 <shreemathi005@gmail.com>
2026-02-17 21:33:03 +0530

    File updated at 2/17/2026, 9:33:03 PM

--- 392-IsSubsequence/392-IsSubsequence.java
+++ 392-IsSubsequence/392-IsSubsequence.java
@@ -1,21 +1,22 @@
-// Last updated: 2/17/2026, 9:28:55 PM
+// Last updated: 2/17/2026, 9:33:03 PM
 1class Solution 
 2{
 3    public boolean isSubsequence(String s, String t)
 4    {
-5        if(s.equals("") || s==null || s.length()==0) return true;
-6        int index =0;
-7        for(int it = 0;it < t.length();it++)
-8        {
-9            if(s.charAt(index) == t.charAt(it))
-10            {
-11                index++;
-12            }
-13            if(index == s.length())
-14            {
-15                return true;
-16            }
-17        }
-18        return false;
-19    }
-20}        
+5        if(  s==null || s.length()==0) return true; //s.equals("") instd of 
+6        //s.length()==0 can also be used
+7        int index =0;
+8        for(int it = 0;it < t.length();it++)
+9        {
+10            if(s.charAt(index) == t.charAt(it))
+11            {
+12                index++;
+13            }
+14            if(index == s.length())
+15            {
+16                return true;
+17            }
+18        }
+19        return false;
+20    }
+21}        
