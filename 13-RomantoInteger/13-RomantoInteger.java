83f198681da0d49eed5a5c95ebfc5d335e789c7e
shreemathi-1 <shreemathi005@gmail.com>
2026-02-22 20:58:51 +0530

    File updated at 2/22/2026, 8:58:50 PM

--- 13-RomantoInteger/13-RomantoInteger.java
+++ 13-RomantoInteger/13-RomantoInteger.java
@@ -1,31 +1,32 @@
-// Last updated: 2/22/2026, 8:48:45 PM
+// Last updated: 2/22/2026, 8:58:50 PM
 1class Solution 
 2{
 3    public int romanToInt(String s)
 4     {
-5        HashMap<Character,Integer> map = new HashMap<>();
-6        map.put('I',1);
-7        map.put('V',5);
-8        map.put('X',10);
-9        map.put('L',50);
-10        map.put('C',100);
-11        map.put('D',500);
-12        map.put('M',1000);
+5       int[] val = new int[128];
+6        val['I'] = 1;
+7        val['V'] = 5;
+8        val['X'] = 10;
+9        val['L'] = 50;
+10        val['C'] = 100;
+11        val['D'] = 500;
+12        val['M'] = 1000;
 13        char[] arr = s.toCharArray();
 14        int res =0;
-15        int i =0,j=1;
-16        for( i =0,j=1;j<arr.length;i++,j++)
-17        {             
-18            if(map.get(arr[i]) >= map.get(arr[j]))
-19            {
-20                res += map.get(arr[i]) ;
-21            }
-22            else
-23            {
-24                res -= map.get(arr[i]);
-25            }
-26        }
-27        res += map.get(arr[i]);
-28        return res;
-29    }
-30}
+15        int i,j;
+16        for( i =0,j=1;i<arr.length-1;i++,j++)
+17        {  
+18
+19            if( val[s.charAt(i)] >= val[s.charAt(j)])
+20            {
+21                res += val[s.charAt(i)];
+22            }
+23            else
+24            {
+25                res -= val[s.charAt(i)];
+26            }
+27        }
+28        res += val[s.charAt(i)];
+29        return res;
+30    }
+31}
