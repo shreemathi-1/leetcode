861e98a8cf636816587d2134c51d24e04561fc7b
shreemathi-1 <shreemathi005@gmail.com>
2026-02-24 07:05:57 +0530

    File updated at 2/24/2026, 7:05:57 AM

--- 42-TrappingRainWater/42-TrappingRainWater.java
+++ 42-TrappingRainWater/42-TrappingRainWater.java
@@ -1,24 +1,28 @@
-// Last updated: 2/13/2026, 3:17:41 PM
-class Solution 
-{
-    public int trap(int[] height) 
-    {
-        int n = height.length;
-        int l = 0,r = n-1,lMax =0,rMax=0,res =0;
-        while(l<r)
-        {
-            if(height[l] <= height[r] )
-            {
-                lMax = Math.max(lMax , height[l]);
-                res += lMax - height[l++];
-            }
-            else
-            {
-                  rMax = Math.max(rMax , height[r]);
-                 res += rMax - height[r--];
-
-            }
-        }
-        return res;
-    }
-}
+// Last updated: 2/24/2026, 7:05:57 AM
+1class Solution 
+2{
+3    public int trap(int[] height)
+4     {
+5        int len = height.length;
+6        
+7        int water =0;
+8        int[] rightMax = new int[len];
+9        int[] leftMax = new int[len];
+10        leftMax[0] = height[0];
+11        rightMax[len-1] = height[len-1];
+12        for(int i =1;i<len;i++)
+13        {
+14            leftMax[i] = Math.max(height[i],leftMax[i-1]);
+15           
+16        }
+17           for(int i =len -2;i>=0;i--)
+18           {
+19            rightMax[i] = Math.max(height[i],rightMax[i+1]);
+20           }
+21        for(int i =0;i<len;i++)
+22        {
+23            water += Math.min(leftMax[i],rightMax[i]) - height[i];
+24        }
+25        return water;
+26    }
+27}
