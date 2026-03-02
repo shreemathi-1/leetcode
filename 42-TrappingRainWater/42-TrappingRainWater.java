e11c1a39721f46d822054e8a844ed73f2e78a77c
shreemathi-1 <shreemathi005@gmail.com>
2026-03-02 20:46:03 +0530

    File updated at 3/2/2026, 8:46:02 PM

--- 42-TrappingRainWater/42-TrappingRainWater.java
+++ 42-TrappingRainWater/42-TrappingRainWater.java
@@ -1,28 +1,21 @@
-// Last updated: 2/24/2026, 7:05:57 AM
-1class Solution 
-2{
-3    public int trap(int[] height)
-4     {
-5        int len = height.length;
-6        
-7        int water =0;
-8        int[] rightMax = new int[len];
-9        int[] leftMax = new int[len];
-10        leftMax[0] = height[0];
-11        rightMax[len-1] = height[len-1];
-12        for(int i =1;i<len;i++)
-13        {
-14            leftMax[i] = Math.max(height[i],leftMax[i-1]);
-15           
-16        }
-17           for(int i =len -2;i>=0;i--)
-18           {
-19            rightMax[i] = Math.max(height[i],rightMax[i+1]);
-20           }
-21        for(int i =0;i<len;i++)
-22        {
-23            water += Math.min(leftMax[i],rightMax[i]) - height[i];
-24        }
-25        return water;
-26    }
-27}
+// Last updated: 3/2/2026, 8:46:02 PM
+class Solution {
+    public int trap(int[] ar) {
+        int[] leftMax = new int[ar.length];
+        int[] rightMax = new int[ar.length];
+        int maxWater = 0;
+        leftMax[0] = ar[0];
+        rightMax[ar.length - 1] = ar[ar.length-1];
+        for (int i = 1; i < ar.length; i++) {
+            leftMax[i] = Math.max(leftMax[i-1], ar[i]);
+        }
+        for (int i = ar.length - 2; i >= 0; i--) {
+            rightMax[i] = Math.max(rightMax[i+1],ar[i]);
+        }
+
+        for (int i = 1; i < ar.length-1; i++) {
+            maxWater += Math.min(leftMax[i], rightMax[i]) - ar[i];
+        }
+        return maxWater;
+    }
+}
