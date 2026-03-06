2580cec1d81a9ab16662d1bcfe53952a69d8d7be
shreemathi-1 <shreemathi005@gmail.com>
2026-03-06 07:06:48 +0530

    File updated at 3/6/2026, 7:06:48 AM

--- 724-FindPivotIndex/724-FindPivotIndex.java
+++ 724-FindPivotIndex/724-FindPivotIndex.java
@@ -1,26 +1,21 @@
-// Last updated: 3/6/2026, 6:54:17 AM
-1class Solution
-2 {
-3    public int pivotIndex(int[] nums) 
+// Last updated: 3/6/2026, 7:06:48 AM
+1class Solution 
+2{
+3    public String mergeAlternately(String word1, String word2) 
 4    {
-5        int total =0;
-6         for(int i =0;i<nums.length;i++)
-7         {
-8            total += nums[i]; 
-9         }
-10     
-11        int leftSum = 0, rightSum = 0;
-12        for(int i =0;i<nums.length;i++)
-13        {
-14          
-15            rightSum = total - leftSum - nums[i];
-16            if(leftSum == rightSum)
-17            {
-18                
-19                return i;
-20            }
-21              leftSum += nums[i];
-22        }
-23        return -1;
-24    }
-25}
+5        StringBuilder res = new StringBuilder();
+6        int i , j;
+7        for( i =0,j=0;i<word1.length();i++,j++)
+8        {
+9            res.append(word1.charAt(i));
+10            if(j < word2.length())
+11            {
+12                res.append(word2.charAt(j));
+13            }
+14        }
+15        if(j < word2.length())
+16        res.append(word2.substring(j));
+17        // res.append(i,word1.length+1);
+18        return res.toString();
+19    }
+20}
