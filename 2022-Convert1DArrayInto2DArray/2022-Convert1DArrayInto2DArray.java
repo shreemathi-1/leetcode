c4e2aa2c6a5d711a45abf47a4e1fa613f0698858
shreemathi-1 <shreemathi005@gmail.com>
2026-03-07 16:15:01 +0530

    File updated at 3/7/2026, 4:15:01 PM

--- 2022-Convert1DArrayInto2DArray/2022-Convert1DArrayInto2DArray.java
+++ 2022-Convert1DArrayInto2DArray/2022-Convert1DArrayInto2DArray.java
@@ -1,19 +1,12 @@
-// Last updated: 3/7/2026, 4:14:25 PM
-1class Solution 
-2{
-3    public int[][] construct2DArray(int[] original, int m, int n) 
-4    {
-5        if(original.length != m*n)
-6        {
-7            return new int[][]{};
-8        }
-9        int mat[][] = new int[m][n];
-10        for(int i =0;i<original.length;i++)
-11        {
-12            int row = i/n;
-13            int col = i%n;
-14            mat[row][col] = original[i];
-15        }
-16        return mat;
-17    }
-18}
+// Last updated: 3/7/2026, 4:15:01 PM
+class Solution {
+    public int[][] construct2DArray(int[] original, int m, int n) {
+        if (m * n != original.length)
+            return new int[0][0];
+        int[][] ans = new int[m][n];
+        for (int i = 0; i < original.length; i++) {
+            ans[i / n][i % n] = original[i];
+        }
+        return ans;
+    }
+}
