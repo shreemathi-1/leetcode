ec001378d84a1cd07cc8b2e89fe1bd42144dad75
shreemathi-1 <shreemathi005@gmail.com>
2026-03-09 19:36:15 +0530

    File updated at 3/9/2026, 7:36:14 PM

--- 151-ReverseWordsinaString/151-ReverseWordsinaString.java
+++ 151-ReverseWordsinaString/151-ReverseWordsinaString.java
@@ -1,15 +1,15 @@
-// Last updated: 2/17/2026, 6:52:26 PM
+// Last updated: 3/9/2026, 7:36:14 PM
 1class Solution 
 2{
 3    public String reverseWords(String s) 
 4    {
-5        String[] arr = s.trim().split("\\s+");
+5        String[] arr = s.split("\\s+");
 6        StringBuilder sb = new StringBuilder();
-7        for(int i =arr.length-1;i>=0;i--)
+7        for(int i = arr.length-1;i>=0;i--)
 8        {
-9           if(i==0) sb.append( arr[i]) ;
-10           else  sb.append(arr[i]).append(" ");
+9            sb.append(arr[i]).append(" ");
+10            
 11        }
-12        return sb.toString() ;
+12        return sb.toString().trim();
 13    }
 14}
