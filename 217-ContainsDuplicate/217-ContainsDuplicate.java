9b7a7ecde6fe57f43e22179503770760f535d55e
shreemathi-1 <shreemathi005@gmail.com>
2026-03-16 10:27:33 +0530

    File updated at 3/16/2026, 10:27:32 AM

--- 217-ContainsDuplicate/217-ContainsDuplicate.java
+++ 217-ContainsDuplicate/217-ContainsDuplicate.java
@@ -1,13 +1,16 @@
-// Last updated: 2/13/2026, 3:16:31 PM
-class Solution {
-    public boolean containsDuplicate(int[] nums) {
-        Arrays.sort(nums); 
-        for (int i = 0; i < nums.length - 1; i++) {
-                if (nums[i] == nums[i+1]) {
-                    return true;
-                }
-        }
-        return false;
-    }
-}
-
+// Last updated: 3/16/2026, 10:27:32 AM
+1class Solution 
+2{
+3    public boolean containsDuplicate(int[] nums) 
+4    {
+5        HashSet<Integer> set = new HashSet<>();
+6        for(int num : nums)
+7        {
+8            if(!set.add(num))
+9            {
+10                return true;
+11            }
+12        }
+13        return false;
+14    }
+15}
