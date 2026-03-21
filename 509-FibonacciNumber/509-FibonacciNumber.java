36191bec880db5b59d757422f7b71812a19bc64f
shreemathi-1 <shreemathi005@gmail.com>
2026-03-21 13:21:16 +0530

    File updated at 3/21/2026, 1:21:15 PM

--- 509-FibonacciNumber/509-FibonacciNumber.java
+++ 509-FibonacciNumber/509-FibonacciNumber.java
@@ -1,4 +1,4 @@
-// Last updated: 3/21/2026, 1:17:32 PM
+// Last updated: 3/21/2026, 1:21:15 PM
 1class Solution 
 2{
 3   
@@ -6,15 +6,9 @@
 5    {
 6        if(n==0) return 0;
 7        if(n==1) return 1;
-8        int f1 = 0;
-9        int f2 =1;
-10        int f3 = 0;
-11        for(int i =2;i<=n;i++)
-12        {
-13            f3 = f1 + f2;
-14            f1 = f2;
-15            f2 = f3;
-16        }
-17        return f2;
-18    }
-19}
+8        else
+9        {
+10            return fib(n-1)+fib(n-2);
+11        }
+12    }
+13}
