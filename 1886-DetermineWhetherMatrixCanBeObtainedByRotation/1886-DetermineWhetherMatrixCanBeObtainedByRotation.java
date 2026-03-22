ec3f83ae8033cb51b57badf2bcf08cb08cdbcdf3
shreemathi-1 <shreemathi005@gmail.com>
2026-03-22 21:42:41 +0530

    File updated at 3/22/2026, 9:42:41 PM

--- 1886-DetermineWhetherMatrixCanBeObtainedByRotation/1886-DetermineWhetherMatrixCanBeObtainedByRotation.java
+++ 1886-DetermineWhetherMatrixCanBeObtainedByRotation/1886-DetermineWhetherMatrixCanBeObtainedByRotation.java
@@ -1,35 +1,35 @@
-// Last updated: 3/22/2026, 9:42:32 PM
+// Last updated: 3/22/2026, 9:42:41 PM
 1class Solution {
-2
-3    public boolean findRotation(int[][] mat, int[][] target) {
-4        int n = mat.length;
-5        // at most 4 rotations
-6        for (int k = 0; k < 4; k++) {
-7            // rotation operation
-8            for (int i = 0; i < n / 2; i++) {
-9                for (int j = 0; j < (n + 1) / 2; j++) {
-10                    int temp = mat[i][j];
-11                    mat[i][j] = mat[n - 1 - j][i];
-12                    mat[n - 1 - j][i] = mat[n - 1 - i][n - 1 - j];
-13                    mat[n - 1 - i][n - 1 - j] = mat[j][n - 1 - i];
-14                    mat[j][n - 1 - i] = temp;
-15                }
-16            }
-17
-18            if (isEqual(mat, target)) {
-19                return true;
-20            }
-21        }
-22        return false;
-23    }
-24
-25    private boolean isEqual(int[][] mat, int[][] target) {
-26        int n = mat.length;
-27        for (int i = 0; i < n; i++) {
-28            for (int j = 0; j < n; j++) {
-29                if (mat[i][j] != target[i][j]) {
-30                    return false;
-31                }
+2    public boolean findRotation(int[][] mat, int[][] target) {
+3        for (int i = 0; i < 4; i++) {
+4            if (isEqual(mat, target)) return true;
+5            rotate(mat);
+6        }
+7        return false;
+8    }
+9
+10    private void rotate(int[][] mat) {
+11        int n = mat.length;
+12        for (int i = 0; i < n; i++) {
+13            for (int j = i + 1; j < n; j++) {
+14                int temp = mat[i][j];
+15                mat[i][j] = mat[j][i];
+16                mat[j][i] = temp;
+17            }
+18        }
+19        for (int i = 0; i < n; i++) {
+20            for (int j = 0; j < n / 2; j++) {
+21                int temp = mat[i][j];
+22                mat[i][j] = mat[i][n - 1 - j];
+23                mat[i][n - 1 - j] = temp;
+24            }
+25        }
+26    }
+27
+28    private boolean isEqual(int[][] mat, int[][] target) {
+29        for (int i = 0; i < mat.length; i++) {
+30            for (int j = 0; j < mat[0].length; j++) {
+31                if (mat[i][j] != target[i][j]) return false;
 32            }
 33        }
 34        return true;
