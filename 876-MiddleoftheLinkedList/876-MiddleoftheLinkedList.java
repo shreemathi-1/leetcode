04c9b37cae2823ee09009e7ce66f920f706a2fd8
shreemathi-1 <shreemathi005@gmail.com>
2026-03-23 11:25:11 +0530

    File updated at 3/23/2026, 11:25:11 AM

--- 876-MiddleoftheLinkedList/876-MiddleoftheLinkedList.java
+++ 876-MiddleoftheLinkedList/876-MiddleoftheLinkedList.java
@@ -1,26 +1,29 @@
-// Last updated: 3/23/2026, 11:12:11 AM
+// Last updated: 3/23/2026, 11:25:11 AM
 1/**
 2 * Definition for singly-linked list.
-3 * public class ListNode {
+3 * class ListNode {
 4 *     int val;
 5 *     ListNode next;
-6 *     ListNode() {}
-7 *     ListNode(int val) { this.val = val; }
-8 *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-9 * }
-10 */
-11class Solution 
-12{
-13    public ListNode middleNode(ListNode head) 
-14    {
-15        ListNode slow = head;
-16        ListNode fast = head;
-17        while(fast != null && fast.next != null)
-18        {
-19            slow =slow.next;
-20            fast = fast.next.next;
-21        }
-22        return slow;
-23        
-24    }
-25}
+6 *     ListNode(int x) {
+7 *         val = x;
+8 *         next = null;
+9 *     }
+10 * }
+11 */
+12public class Solution {
+13    public boolean hasCycle(ListNode head) {
+14        ListNode slow = head,fast = head;
+15        while(fast != null && fast.next != null)
+16        {
+17           
+18            slow = slow.next;
+19            fast = fast.next.next; 
+20            if(slow == fast)
+21            {
+22                return true;
+23            }
+24
+25        }
+26        return false;
+27    }
+28}
