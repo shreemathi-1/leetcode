b74a3fb5ee26f62e4735a07e923867d530fbe3f7
shreemathi-1 <shreemathi005@gmail.com>
2026-03-24 21:38:34 +0530

    File updated at 3/24/2026, 9:38:33 PM

--- 23-MergekSortedLists/23-MergekSortedLists.java
+++ 23-MergekSortedLists/23-MergekSortedLists.java
@@ -1,33 +1,46 @@
-// Last updated: 3/24/2026, 9:38:11 PM
-1/**
-2 * Definition for singly-linked list.
-3 * public class ListNode {
-4 *     int val;
-5 *     ListNode next;
-6 *     ListNode() {}
-7 *     ListNode(int val) { this.val = val; }
-8 *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-9 * }
-10 */
-11class Solution {
-12    public ListNode mergeKLists(ListNode[] lists) {
-13        ListNode dummyHead = new ListNode(-1);
-14        var pq = new PriorityQueue<ListNode>((a, b) ->  a.val - b.val);
-15
-16        
-17        for(int i = 0; i<lists.length; i++) {
-18            if (lists[i] != null) pq.add(lists[i]);
-19        }
-20        ListNode ptr = dummyHead;
-21
-22        while(!pq.isEmpty()) {
-23            var nextNode = pq.poll();
-24            if (nextNode.next != null) {pq.add(nextNode.next);}
-25            ptr.next = nextNode;
-26            ptr = nextNode;
-27            ptr.next = null;
-28        }
-29
-30        return dummyHead.next;
-31    }
-32}
+// Last updated: 3/24/2026, 9:38:33 PM
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        PriorityQueue<ListNode> pq = new PriorityQueue<>((a, b) -> a.val - b.val);// declare sorting order here
+
+        for (int i = 0; i < lists.length; i++) {
+            if (lists[i] != null) {
+                pq.offer(lists[i]);
+            }
+        }
+
+        if (pq.size() == 1) {
+            return pq.poll();
+        }
+
+        ListNode dummy = new ListNode(0);
+        dummy.next = pq.peek();
+
+        ListNode curr = dummy.next;
+
+        // While the priority queue isn't empty, get the peek node and add that to new list, and advance it
+        while (!pq.isEmpty()) {
+            ListNode poll = pq.poll();
+
+            if (poll.next != null) {
+                pq.offer(poll.next);
+            }
+
+            // Add into list and advance
+            curr.next = poll;
+            curr = curr.next;
+        }
+
+        return dummy.next;
+    }
+}
