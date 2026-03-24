0cf7b6b4e30b809dbc78e385a6800f3b56cb86a9
shreemathi-1 <shreemathi005@gmail.com>
2026-03-24 21:38:12 +0530

    File updated at 3/24/2026, 9:38:11 PM

--- 23-MergekSortedLists/23-MergekSortedLists.java
+++ 23-MergekSortedLists/23-MergekSortedLists.java
@@ -1,39 +1,33 @@
-// Last updated: 3/24/2026, 8:37:06 PM
-1class Solution {
-2    public ListNode mergeKLists(ListNode[] lists) {
-3        if (lists == null || lists.length == 0) {
-4            return null;
-5        }
-6
-7        while (lists.length > 1) {
-8            List<ListNode> temp = new ArrayList<>();
-9            for (int i = 0; i < lists.length; i += 2) {
-10                ListNode l1 = lists[i];
-11                ListNode l2 = i + 1 < lists.length ? lists[i + 1] : null;
-12                temp.add(mergeLists(l1, l2));
-13            }
-14            lists = temp.toArray(new ListNode[0]);
-15        }
-16
-17        return lists[0];        
-18    }
-19
-20    private ListNode mergeLists(ListNode l1, ListNode l2) {
-21        ListNode node = new ListNode();
-22        ListNode ans = node;
-23
-24        while (l1 != null && l2 != null) {
-25            if (l1.val > l2.val) {
-26                node.next = l2;
-27                l2 = l2.next;
-28            } else {
-29                node.next = l1;
-30                l1 = l1.next;
-31            }
-32            node = node.next;
-33        }
-34
-35        node.next = l1 != null ? l1 : l2;
-36        return ans.next;
-37    }    
-38}
+// Last updated: 3/24/2026, 9:38:11 PM
+1/**
+2 * Definition for singly-linked list.
+3 * public class ListNode {
+4 *     int val;
+5 *     ListNode next;
+6 *     ListNode() {}
+7 *     ListNode(int val) { this.val = val; }
+8 *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+9 * }
+10 */
+11class Solution {
+12    public ListNode mergeKLists(ListNode[] lists) {
+13        ListNode dummyHead = new ListNode(-1);
+14        var pq = new PriorityQueue<ListNode>((a, b) ->  a.val - b.val);
+15
+16        
+17        for(int i = 0; i<lists.length; i++) {
+18            if (lists[i] != null) pq.add(lists[i]);
+19        }
+20        ListNode ptr = dummyHead;
+21
+22        while(!pq.isEmpty()) {
+23            var nextNode = pq.poll();
+24            if (nextNode.next != null) {pq.add(nextNode.next);}
+25            ptr.next = nextNode;
+26            ptr = nextNode;
+27            ptr.next = null;
+28        }
+29
+30        return dummyHead.next;
+31    }
+32}
