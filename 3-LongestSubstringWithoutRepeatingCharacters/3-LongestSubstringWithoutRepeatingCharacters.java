23b581fe7cc96f22174d4fe88eb8e9ccafb44a9f
shreemathi-1 <shreemathi005@gmail.com>
2026-03-30 21:35:09 +0530

    File updated at 3/30/2026, 9:35:08 PM

--- 3-LongestSubstringWithoutRepeatingCharacters/3-LongestSubstringWithoutRepeatingCharacters.java
+++ 3-LongestSubstringWithoutRepeatingCharacters/3-LongestSubstringWithoutRepeatingCharacters.java
@@ -1,31 +1,25 @@
-// Last updated: 3/29/2026, 11:25:10 PM
-class Solution {
-    public int lengthOfLongestSubstring(String s) {
-        int maxlength= 0;
-        Set<Character> charset = new HashSet<>();
-        int left = 0;
-
-        for(int right= 0;right<s.length();right++){
-            if(!charset.contains(s.charAt(right))){
-                charset.add(s.charAt(right));
-                maxlength = Math.max(maxlength, right-left+1);
-            }else{
-                while(charset.contains(s.charAt(right))){
-                    charset.remove(s.charAt(left));
-                    left++;
-                }
-                charset.add(s.charAt(right));
-            }
-        }
-        return maxlength;
-    }
-    static {
-        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-            try (java.io.FileWriter fw = new java.io.FileWriter("display_runtime.txt")) {
-                //this handles the time writing
-                fw.write("0");
-            } catch (Exception e) {
-            }
-        }));
-    }
-}
+// Last updated: 3/30/2026, 9:35:08 PM
+1class Solution 
+2{
+3    public int lengthOfLongestSubstring(String s) 
+4    {
+5        LinkedHashSet set = new LinkedHashSet<>();
+6        int i =0,max =0,start =0;
+7        while(i<s.length())
+8        {
+9            if(!set.contains(s.charAt(i)))
+10            {
+11                
+12                set.add(s.charAt(i));
+13                max = Math.max(max,set.size());
+14                i++;
+15            }
+16            else
+17            {
+18                set.remove(s.charAt(start));
+19                start++;
+20            }
+21        }
+22        return max;
+23    }
+24}
