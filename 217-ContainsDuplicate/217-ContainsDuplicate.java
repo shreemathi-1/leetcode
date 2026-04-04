538acb9b4f7df754663411854915b40dc1d0f035
shreemathi-1 <shreemathi005@gmail.com>
2026-04-04 15:57:06 +0530

    File updated at 4/4/2026, 3:57:06 PM

--- 217-ContainsDuplicate/217-ContainsDuplicate.java
+++ 217-ContainsDuplicate/217-ContainsDuplicate.java
@@ -1,16 +1,20 @@
-// Last updated: 3/16/2026, 10:27:32 AM
+// Last updated: 4/4/2026, 3:57:06 PM
 1class Solution 
 2{
 3    public boolean containsDuplicate(int[] nums) 
 4    {
 5        HashSet<Integer> set = new HashSet<>();
-6        for(int num : nums)
+6        for(int i =0;i<nums.length;i++)
 7        {
-8            if(!set.add(num))
+8            if(set.contains(nums[i]))
 9            {
 10                return true;
 11            }
-12        }
-13        return false;
-14    }
-15}
+12            else
+13            {
+14                set.add(nums[i]);
+15            }
+16        }
+17        return false;
+18    }
+19}
