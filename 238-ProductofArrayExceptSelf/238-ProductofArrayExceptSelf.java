ee8bf8bca781ef184eebc42fa6951f93aaaa832a
shreemathi-1 <shreemathi005@gmail.com>
2026-04-09 20:46:08 +0530

    File updated at 4/9/2026, 8:46:07 PM

--- 238-ProductofArrayExceptSelf/238-ProductofArrayExceptSelf.java
+++ 238-ProductofArrayExceptSelf/238-ProductofArrayExceptSelf.java
@@ -1,22 +1,21 @@
-// Last updated: 2/15/2026, 12:32:27 PM
+// Last updated: 4/9/2026, 8:46:07 PM
 1class Solution 
 2{
-3    public int[] productExceptSelf(int[] nums)
-4     {
-5        int n = nums.length;
-6        int[] res = new int[n];
-7        int temp=1;
-8        for(int i =0;i<n;i++)
-9        {
-10            res[i] = temp;
-11            temp *= nums[i];
-12        }
-13        temp = 1;
-14        for(int i =n-1;i>=0;i--)
-15        {
-16            res[i] = res[i]*temp;
-17            temp *= nums[i];
-18        }
-19        return res;
-20    }
-21}
+3    public int[] productExceptSelf(int[] nums) 
+4    {
+5        int[] ans = new int[nums.length];
+6        int temp1 = 1;
+7        for(int i =0;i<nums.length;i++)
+8        {
+9            ans[i] = temp1;
+10            temp1 *= nums[i];
+11        }
+12        int temp2 =1;
+13        for(int i = nums.length-1;i>=0;i--)
+14        {
+15            ans[i] *=temp2;
+16            temp2 *= nums[i] ;
+17        }
+18        return ans;
+19    }
+20}
