38a105df64eeb61cf32aaabf5672bb540b27a286
shreemathi-1 <shreemathi005@gmail.com>
2026-04-15 21:06:15 +0530

    File updated at 4/15/2026, 9:06:15 PM

--- 414-ThirdMaximumNumber/414-ThirdMaximumNumber.java
+++ 414-ThirdMaximumNumber/414-ThirdMaximumNumber.java
@@ -1,23 +1,14 @@
-// Last updated: 4/15/2026, 8:55:16 PM
-1class Solution 
-2{
-3    public boolean isAnagram(String s, String t) 
-4    {
-5        if(s.length() != t.length())
-6        {
-7            return false;
-8        }
-9        char[] arr1 = s.toCharArray();
-10        char[] arr2 = t.toCharArray();
-11        Arrays.sort(arr1);
-12        Arrays.sort(arr2);
-13        for(int i =0;i<arr1.length;i++)
-14        {
-15            if(arr1[i] != arr2[i])
-16            {
-17                return false;
-18            }
-19        }
-20        return true;
-21    }
-22}
+// Last updated: 4/15/2026, 9:06:15 PM
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        if(strs.length == 0) return "";
+        String prefix = strs[0];
+        for(int i = 1; i < strs.length; i++) {
+            while(strs[i].indexOf(prefix) != 0) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+                if(prefix.isEmpty()) return "";
+            }
+        }
+        return prefix;
+    }
+}
