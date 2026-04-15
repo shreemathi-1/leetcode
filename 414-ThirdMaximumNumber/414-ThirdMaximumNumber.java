fd16cd36fea046dfa43030b8820e401fb90a11d6
shreemathi-1 <shreemathi005@gmail.com>
2026-04-15 20:55:17 +0530

    File updated at 4/15/2026, 8:55:16 PM

--- 414-ThirdMaximumNumber/414-ThirdMaximumNumber.java
+++ 414-ThirdMaximumNumber/414-ThirdMaximumNumber.java
@@ -1,33 +1,23 @@
-// Last updated: 4/15/2026, 8:47:02 PM
+// Last updated: 4/15/2026, 8:55:16 PM
 1class Solution 
 2{
-3    public boolean isPalindrome(String s) 
+3    public boolean isAnagram(String s, String t) 
 4    {
-5        s = s.toLowerCase();
-6        StringBuilder sb = new StringBuilder();
-7       for(int i =0;i<s.length();i++)
-8       {
-9         if(s.charAt(i) >= 'a' && s.charAt(i) <='z')
-10         {
-11            sb.append(s.charAt(i));
-12         }
-13         if(s.charAt(i) >='0' && s.charAt(i) <= '9')
-14         {
-15            sb.append(s.charAt(i));
-16         }
-17       }
-18       String res = sb.toString();
-19        System.out.println(res);
-20        int l =0,r =res.length()-1;
-21        while(l<r)
-22        {
-23           if(res.charAt(l) != res.charAt(r))
-24           {
-25            return false;
-26           } 
-27           l++;
-28           r--;
-29        }
-30        return true;
-31    }
-32}
+5        if(s.length() != t.length())
+6        {
+7            return false;
+8        }
+9        char[] arr1 = s.toCharArray();
+10        char[] arr2 = t.toCharArray();
+11        Arrays.sort(arr1);
+12        Arrays.sort(arr2);
+13        for(int i =0;i<arr1.length;i++)
+14        {
+15            if(arr1[i] != arr2[i])
+16            {
+17                return false;
+18            }
+19        }
+20        return true;
+21    }
+22}
