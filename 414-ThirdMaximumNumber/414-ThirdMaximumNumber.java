106e59015c286cf97ee1ae6a7a0fd68a9e50731f
shreemathi-1 <shreemathi005@gmail.com>
2026-04-15 20:47:04 +0530

    File updated at 4/15/2026, 8:47:02 PM

--- 414-ThirdMaximumNumber/414-ThirdMaximumNumber.java
+++ 414-ThirdMaximumNumber/414-ThirdMaximumNumber.java
@@ -1,12 +1,33 @@
-// Last updated: 4/15/2026, 8:33:36 PM
-1class Solution {
-2    public int thirdMax(int[] nums) {
-3        int count = 0;
-4        Arrays.sort(nums);
-5        for(int i = nums.length - 1; i > 0; i--){
-6            if(nums[i] != nums[i - 1]) count++;
-7            if(count == 2) return nums[i - 1];
-8        }
-9        return nums[nums.length - 1];
-10    }
-11}
+// Last updated: 4/15/2026, 8:47:02 PM
+1class Solution 
+2{
+3    public boolean isPalindrome(String s) 
+4    {
+5        s = s.toLowerCase();
+6        StringBuilder sb = new StringBuilder();
+7       for(int i =0;i<s.length();i++)
+8       {
+9         if(s.charAt(i) >= 'a' && s.charAt(i) <='z')
+10         {
+11            sb.append(s.charAt(i));
+12         }
+13         if(s.charAt(i) >='0' && s.charAt(i) <= '9')
+14         {
+15            sb.append(s.charAt(i));
+16         }
+17       }
+18       String res = sb.toString();
+19        System.out.println(res);
+20        int l =0,r =res.length()-1;
+21        while(l<r)
+22        {
+23           if(res.charAt(l) != res.charAt(r))
+24           {
+25            return false;
+26           } 
+27           l++;
+28           r--;
+29        }
+30        return true;
+31    }
+32}
