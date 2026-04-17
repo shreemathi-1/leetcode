7f3902c29e6e75a67459381809c0e84d0361d46e
shreemathi-1 <shreemathi005@gmail.com>
2026-04-17 11:41:44 +0530

    File updated at 4/17/2026, 11:41:44 AM

--- 387-FirstUniqueCharacterinaString/387-FirstUniqueCharacterinaString.java
+++ 387-FirstUniqueCharacterinaString/387-FirstUniqueCharacterinaString.java
@@ -1,20 +1,20 @@
-// Last updated: 3/16/2026, 1:39:06 PM
+// Last updated: 4/17/2026, 11:41:44 AM
 1class Solution 
 2{
 3    public int firstUniqChar(String s) 
 4    {
-5       
-6        HashMap<Character,Integer> map = new LinkedHashMap<>();
-7        for(int i =0;i<s.length();i++)
-8        {
-9           map.put(s.charAt(i),map.getOrDefault(s.charAt(i),0)+1);
-10        }
-11        for(int i =0;i<s.length();i++)
-12        {
-13            if(map.get(s.charAt(i)) ==1)
-14            return i;
-15        }
-16       
+5        int[] freq = new int[26];
+6        for(int i =0;i<s.length();i++)
+7        {
+8            freq[s.charAt(i) - 'a']++;
+9        }
+10        for(int i =0;i< s.length();i++)
+11        {
+12            if(freq[s.charAt(i)- 'a'] ==1)
+13            {
+14                return i;
+15            }
+16        }
 17        return -1;
 18    }
 19}
