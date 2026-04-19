1d18f8086ced1fa35aeb7b1c24b48c9795b82037
shreemathi-1 <shreemathi005@gmail.com>
2026-04-19 20:16:02 +0530

    File updated at 4/19/2026, 8:16:02 PM

--- 13-RomantoInteger/13-RomantoInteger.java
+++ 13-RomantoInteger/13-RomantoInteger.java
@@ -1,32 +1,31 @@
-// Last updated: 2/22/2026, 8:58:50 PM
-1class Solution 
-2{
-3    public int romanToInt(String s)
-4     {
-5       int[] val = new int[128];
-6        val['I'] = 1;
-7        val['V'] = 5;
-8        val['X'] = 10;
-9        val['L'] = 50;
-10        val['C'] = 100;
-11        val['D'] = 500;
-12        val['M'] = 1000;
-13        char[] arr = s.toCharArray();
-14        int res =0;
-15        int i,j;
-16        for( i =0,j=1;i<arr.length-1;i++,j++)
-17        {  
-18
-19            if( val[s.charAt(i)] >= val[s.charAt(j)])
-20            {
-21                res += val[s.charAt(i)];
-22            }
-23            else
-24            {
-25                res -= val[s.charAt(i)];
-26            }
-27        }
-28        res += val[s.charAt(i)];
-29        return res;
-30    }
-31}
+// Last updated: 4/19/2026, 8:16:02 PM
+1class Solution {
+2    public int romanToInt(String s) {
+3       int total = 0;
+4
+5        for (int i = 0; i < s.length(); i++) {
+6            int current = value(s.charAt(i));
+7
+8            if (i + 1 < s.length() && current < value(s.charAt(i + 1))) {
+9                total -= current;
+10            } else {
+11                total += current;
+12            }
+13        }
+14
+15        return total;
+16    }
+17
+18    private int value(char ch) {
+19        switch (ch) {
+20            case 'I': return 1;
+21            case 'V': return 5;
+22            case 'X': return 10;
+23            case 'L': return 50;
+24            case 'C': return 100;
+25            case 'D': return 500;
+26            case 'M': return 1000;
+27            default: return 0;
+28        }
+29    }
+30}
