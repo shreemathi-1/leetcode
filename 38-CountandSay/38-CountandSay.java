44062e928998bd93bed42b8d26d76919b755b76c
shreemathi-1 <shreemathi005@gmail.com>
2026-04-24 19:38:56 +0530

    File updated at 4/24/2026, 7:38:55 PM

--- 38-CountandSay/38-CountandSay.java
+++ 38-CountandSay/38-CountandSay.java
@@ -1,33 +1,21 @@
-// Last updated: 4/24/2026, 7:34:40 PM
-class Solution {
-    public String reverseWords(String s) {
-        String[] splitStr = s.split("\\s+");
-		String[] newStr = new String[splitStr.length];
-		
-		for(int i=0; i<splitStr.length; i++)
-		{
-			newStr[i]=reversed(splitStr[i]);
-		}
-		
-		StringBuilder newbldr = new StringBuilder();
-		
-		for(int j=0; j<newStr.length; j++)
-		{
-			newbldr.append(newStr[j]);
-			if(j!=newStr.length-1)
-				newbldr.append(" ");
-		}
-		
-		return newbldr.toString();
-    }
-    public String reversed(String sn)
-	{
-		StringBuilder sbr = new StringBuilder();
-		
-		for(int i=sn.length()-1; i>=0; i--)
-		{
-			sbr.append(sn.charAt(i));
-		}
-		return sbr.toString();
-	}
-}
+// Last updated: 4/24/2026, 7:38:55 PM
+1class Solution {
+2    public boolean isIsomorphic(String s, String t) {      
+3        int[] indexS = new int[200]; 
+4        int[] indexT = new int[200]; 
+5        int len = s.length();
+6        if(len != t.length()) {
+7            return false;
+8        }
+9        for(int i = 0; i < len; i++) 
+10        {
+11            if(indexS[s.charAt(i)] != indexT[t.charAt(i)]) 
+12            {
+13                return false; 
+14            }
+15            indexS[s.charAt(i)] = i + 1; 
+16            indexT[t.charAt(i)] = i + 1;
+17        }
+18        return true;
+19    }
+20}
