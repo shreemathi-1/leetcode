c9f85927cfe79b79019ab784fc1f091d102dbf55
shreemathi-1 <shreemathi005@gmail.com>
2026-04-24 19:34:41 +0530

    File updated at 4/24/2026, 7:34:40 PM

--- 38-CountandSay/38-CountandSay.java
+++ 38-CountandSay/38-CountandSay.java
@@ -1,14 +1,33 @@
-// Last updated: 4/24/2026, 7:32:30 PM
+// Last updated: 4/24/2026, 7:34:40 PM
 class Solution {
     public String reverseWords(String s) {
-        String[] words = s.split(" ");
-        StringBuilder result = new StringBuilder();
-        
-        for (String word : words) {
-            StringBuilder temp = new StringBuilder(word);
-            result.append(temp.reverse().toString()).append(" ");
-        }
-        
-        return result.toString().trim();
+        String[] splitStr = s.split("\\s+");
+		String[] newStr = new String[splitStr.length];
+		
+		for(int i=0; i<splitStr.length; i++)
+		{
+			newStr[i]=reversed(splitStr[i]);
+		}
+		
+		StringBuilder newbldr = new StringBuilder();
+		
+		for(int j=0; j<newStr.length; j++)
+		{
+			newbldr.append(newStr[j]);
+			if(j!=newStr.length-1)
+				newbldr.append(" ");
+		}
+		
+		return newbldr.toString();
     }
+    public String reversed(String sn)
+	{
+		StringBuilder sbr = new StringBuilder();
+		
+		for(int i=sn.length()-1; i>=0; i--)
+		{
+			sbr.append(sn.charAt(i));
+		}
+		return sbr.toString();
+	}
 }
