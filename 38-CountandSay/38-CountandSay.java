d94a26acffb8f66965625ffecb13c28fa92a6f7f
shreemathi-1 <shreemathi005@gmail.com>
2026-04-24 19:31:25 +0530

    File updated at 4/24/2026, 7:31:25 PM

--- 38-CountandSay/38-CountandSay.java
+++ 38-CountandSay/38-CountandSay.java
@@ -1,27 +1,28 @@
-// Last updated: 4/24/2026, 7:14:35 PM
-1class Solution {
-2    public String countAndSay(int n) {
-3        String result = "1";
-4        for (int i = 1; i < n; i++) {
-5            result = describe(result);
-6        }
-7        return result;
-8    }
-9
-10    private String describe(String s) {
-11        StringBuilder sb = new StringBuilder();
-12        int count = 1;
-13
-14        for (int i = 1; i < s.length(); i++) {
-15            if (s.charAt(i) == s.charAt(i - 1)) {
-16                count++;
-17            } else {
-18                sb.append(count).append(s.charAt(i - 1));
-19                count = 1;
-20            }
-21        }
-22
-23        sb.append(count).append(s.charAt(s.length() - 1));
-24        return sb.toString();
-25    }
-26}
+// Last updated: 4/24/2026, 7:31:25 PM
+1class Solution 
+2{
+3    public String reverseWords(String s) 
+4    {
+5        StringBuilder rev = new StringBuilder();
+6        String[] arr = s.split("\\s+");
+7        for(int i =0;i<arr.length;i++)
+8        {
+9            char[] let = arr[i].toCharArray();
+10            int l = 0;
+11            int r = arr[i].length()-1;
+12            while(l<r)
+13            {
+14                char temp = let[l];
+15                let[l] = let[r];
+16                let[r] = temp;
+17                l++;
+18                r--;
+19            }
+20            if(i == arr.length-1)
+21             rev.append(new String(let));
+22             else
+23            rev.append(new String(let)).append(" ");
+24        }
+25        return rev.toString();
+26    }
+27}
