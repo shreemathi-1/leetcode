d9ef474780f494120f900ccb73982130a052d37e
shreemathi-1 <shreemathi005@gmail.com>
2026-04-26 17:41:16 +0530

    File updated at 4/26/2026, 5:41:16 PM

--- 141-LinkedListCycle/141-LinkedListCycle.java
+++ 141-LinkedListCycle/141-LinkedListCycle.java
@@ -1,30 +1,31 @@
-// Last updated: 2/13/2026, 3:17:00 PM
-/**
- * Definition for singly-linked list.
- * class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) {
- *         val = x;
- *         next = null;
- *     }
- * }
- */
-public class Solution {
-    public boolean hasCycle(ListNode head) 
-    {
-        
-        ListNode slow = head,fast = head;
-        while(fast != null && fast.next !=null)
-        {
-            slow = slow.next;
-            fast = fast.next.next;
-            if(fast == slow)
-            {
-                return true;
-            }
-            
-        }
-        return false;
-    }
-}
+// Last updated: 4/26/2026, 5:41:16 PM
+1/**
+2 * Definition for singly-linked list.
+3 * class ListNode {
+4 *     int val;
+5 *     ListNode next;
+6 *     ListNode(int x) {
+7 *         val = x;
+8 *         next = null;
+9 *     }
+10 * }
+11 */
+12public class Solution 
+13{
+14    public boolean hasCycle(ListNode head) 
+15    {
+16        ListNode slow = head , fast = head;
+17        while(fast!=null && fast.next != null)
+18        {
+19            
+20            slow = slow.next;
+21            fast = fast.next.next;
+22            if(slow == fast)
+23            {
+24                return true;
+25            }
+26            
+27        }
+28        return false;   
+29    }
+30}
