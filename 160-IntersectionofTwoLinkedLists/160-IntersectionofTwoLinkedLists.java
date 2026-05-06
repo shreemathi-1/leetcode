cf2e0e5960524df361a833e111d419352b83acab
shreemathi-1 <shreemathi005@gmail.com>
2026-05-06 11:23:18 +0530

    File updated at 5/6/2026, 11:23:17 AM

--- 160-IntersectionofTwoLinkedLists/160-IntersectionofTwoLinkedLists.java
+++ 160-IntersectionofTwoLinkedLists/160-IntersectionofTwoLinkedLists.java
@@ -1,17 +1,32 @@
-// Last updated: 5/6/2026, 11:22:12 AM
+// Last updated: 5/6/2026, 11:23:17 AM
 1/**
 2 * Definition for singly-linked list.
 3 * public class ListNode {
 4 *     int val;
 5 *     ListNode next;
-6 *     ListNode(int x) { val = x; }
-7 * }
-8 */
-9class Solution 
-10{
-11    public void deleteNode(ListNode node) 
-12    {
-13        node.val = node.next.val;
-14        node.next = node.next.next;
-15    }
-16}
+6 *     ListNode() {}
+7 *     ListNode(int val) { this.val = val; }
+8 *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+9 * }
+10 */
+11class Solution {
+12    public ListNode oddEvenList(ListNode head) {
+13        if(head == null || head.next == null)
+14        {
+15            return head;
+16        }
+17        ListNode odd = head;
+18        ListNode even = odd.next;
+19        ListNode evenHead = even;
+20        
+21        while( even != null && even.next != null)
+22        {
+23            odd.next = even.next;
+24          odd = odd.next;
+25          even.next = odd.next;
+26          even = even.next;
+27        }
+28         odd.next = evenHead;
+29          return head;
+30    }
+31}
