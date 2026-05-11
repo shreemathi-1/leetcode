379f19b0e7d0b74d5a16a1df0725d6a189084c79
shreemathi-1 <shreemathi005@gmail.com>
2026-05-11 09:35:12 +0530

    File updated at 5/11/2026, 9:35:12 AM

--- 24-SwapNodesinPairs/24-SwapNodesinPairs.java
+++ 24-SwapNodesinPairs/24-SwapNodesinPairs.java
@@ -1,16 +1,22 @@
-// Last updated: 5/11/2026, 9:34:03 AM
+// Last updated: 5/11/2026, 9:35:12 AM
 1class Solution {
-2    public ListNode removeElements(ListNode head, int val) {
-3        ListNode ans = new ListNode(0, head);
-4        ListNode dummy = ans;
-5
-6        while (dummy != null) {
-7            while (dummy.next != null && dummy.next.val == val) {
-8                dummy.next = dummy.next.next;
-9            }
-10            dummy = dummy.next;
-11        }
-12
-13        return ans.next;        
-14    }
-15}
+2    public boolean isValid(String s) {
+3        Stack<Character> stack = new Stack<>();
+4        Map<Character, Character> mapping = new HashMap<>();
+5        mapping.put(')','(');
+6        mapping.put('}','{'); 
+7        mapping.put(']','[');  
+8
+9
+10        for (char c : s.toCharArray()) {
+11            if (mapping.containsValue(c)) {
+12                stack.push(c);
+13            } else if (mapping.containsKey(c)) {
+14                if (stack.isEmpty() || mapping.get(c) != stack.pop()) {
+15                    return false;
+16                }
+17            }
+18        }
+19        return stack.isEmpty();
+20    }
+21}
