1e60b2a36fd1b258c98fc035efc05229156613a6
shreemathi-1 <shreemathi005@gmail.com>
2026-05-11 09:39:47 +0530

    File updated at 5/11/2026, 9:39:47 AM

--- 24-SwapNodesinPairs/24-SwapNodesinPairs.java
+++ 24-SwapNodesinPairs/24-SwapNodesinPairs.java
@@ -1,47 +1,83 @@
-// Last updated: 5/11/2026, 9:38:48 AM
-1class MyQueue {
+// Last updated: 5/11/2026, 9:39:47 AM
+1class MyStack {
 2
-3    Stack<Integer> st1 ;
-4    Stack<Integer> st2;
-5    public MyQueue() {
-6        st1 = new Stack<>();
-7        st2 = new Stack<>();
+3Queue<Integer> q1;
+4Queue<Integer> q2;
+5    public MyStack() {
+6        q1 = new LinkedList<>();
+7        q2 = new LinkedList<>();
 8    }
 9    
-10    public void push(int x) 
-11    {
-12        while(!st1.isEmpty())
-13        {
-14            st2.push(st1.pop());
-15        }
-16        st1.push(x);
-17        while(!st2.isEmpty())
-18        {
-19            st1.push(st2.pop());
-20        }
-21    }
-22    
-23    public int pop() 
-24    {
-25        
-26            return st1.pop();  
+10    public void push(int x) {
+11        if(q1.isEmpty())
+12        {
+13            q1.offer(x);
+14        }
+15        else
+16        {
+17            while(!q1.isEmpty())
+18            {
+19               q2.offer( q1.poll());
+20            }
+21            q1.offer(x);
+22            while(!q2.isEmpty())
+23            {
+24                q1.offer(q2.poll());
+25            }
+26        }
 27    }
 28    
-29    public int peek() {
-30        
-31        return st1.peek();
-32    }
-33    
-34    public boolean empty() {
-35        return st1.isEmpty() ;
-36    }
-37}
-38
-39/**
-40 * Your MyQueue object will be instantiated and called as such:
-41 * MyQueue obj = new MyQueue();
-42 * obj.push(x);
-43 * int param_2 = obj.pop();
-44 * int param_3 = obj.peek();
-45 * boolean param_4 = obj.empty();
-46 */
+29    public int pop() {
+30        return q1.poll();
+31    }
+32    
+33    public int top() {
+34        return q1.peek();
+35    }
+36    
+37    public boolean empty() {
+38        return q1.isEmpty();
+39    }
+40}
+41
+42/**
+43 * Your MyStack object will be instantiated and called as such:
+44 * MyStack obj = new MyStack();
+45 * obj.push(x);
+46 * int param_2 = obj.pop();
+47 * int param_3 = obj.top();
+48 * boolean param_4 = obj.empty();
+49 */
+50
+51// class MyStack
+52// {
+53
+54// Queue<Integer> q;
+55
+56//     public MyStack() {
+57//         q = new LinkedList<>();
+58       
+59//     }
+60    
+61//     public void push(int x) {
+62//              q.offer(x);
+63//            for(int i =0;i<q.size()-1;i++)
+64//             {
+65//                q.offer( q.poll());
+66//             }
+67        
+68//     }
+69    
+70//     public int pop() {
+71//         return q.poll();
+72//     }
+73    
+74//     public int top() {
+75//         return q.peek();
+76//     }
+77    
+78//     public boolean empty() {
+79//         return q.isEmpty();
+80//     }
+81// }
+82
