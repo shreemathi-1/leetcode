6e50073af3f6be5868a93af64e777e4804435152
shreemathi-1 <shreemathi005@gmail.com>
2026-05-25 21:05:57 +0530

    File updated at 5/25/2026, 9:05:57 PM

--- 328-OddEvenLinkedList/328-OddEvenLinkedList.java
+++ 328-OddEvenLinkedList/328-OddEvenLinkedList.java
@@ -1,27 +1,21 @@
-// Last updated: 5/25/2026, 9:04:25 PM
-1class Solution 
-2{
-3    public int[] intersection(int[] nums1, int[] nums2) 
-4    {       
-5        HashSet<Integer> set = new HashSet<>();
-6        HashSet<Integer> resSet = new HashSet<>();
-7        for(int num : nums1)
-8        {
-9            set.add(num);
-10        }
-11        for(int num : nums2)
-12        {
-13            if(set.contains(num))
-14            {
-15               resSet.add (num);
-16            }
+// Last updated: 5/25/2026, 9:05:57 PM
+1class Solution {
+2    public int[] intersection(int[] nums1, int[] nums2) {
+3        int[] seen = new int[1001];
+4        for(int num : nums1) {
+5            seen[num] = 1;
+6        }
+7        ArrayList<Integer> res = new ArrayList<>();
+8        for(int num : nums2) {
+9            if (seen[num] == 1) {
+10                res.add(num);
+11                seen[num] = 0;
+12            }
+13        }
+14        int[] ans = new int [res.size()];
+15        for (int i = 0; i < res.size(); i++) {
+16            ans[i] = res.get(i);
 17        }
-18       int[] arr = new int[resSet.size()];
-19       int i =0;
-20       for(int ele : resSet)
-21       {
-22            arr[i++] = ele;
-23       }
-24       return arr;
-25    }
-26}
+18        return ans;
+19    }
+20}
