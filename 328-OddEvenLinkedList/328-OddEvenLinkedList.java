49a1d2b113871cbe4e58da48eec556ca63f87abe
shreemathi-1 <shreemathi005@gmail.com>
2026-05-25 21:07:17 +0530

    File updated at 5/25/2026, 9:07:16 PM

--- 328-OddEvenLinkedList/328-OddEvenLinkedList.java
+++ 328-OddEvenLinkedList/328-OddEvenLinkedList.java
@@ -1,21 +1,24 @@
-// Last updated: 5/25/2026, 9:05:57 PM
-1class Solution {
-2    public int[] intersection(int[] nums1, int[] nums2) {
-3        int[] seen = new int[1001];
-4        for(int num : nums1) {
-5            seen[num] = 1;
-6        }
-7        ArrayList<Integer> res = new ArrayList<>();
-8        for(int num : nums2) {
-9            if (seen[num] == 1) {
-10                res.add(num);
-11                seen[num] = 0;
-12            }
-13        }
-14        int[] ans = new int [res.size()];
-15        for (int i = 0; i < res.size(); i++) {
-16            ans[i] = res.get(i);
-17        }
-18        return ans;
-19    }
-20}
+// Last updated: 5/25/2026, 9:07:16 PM
+1class Solution 
+2{
+3    public int subarraySum(int[] nums, int k) 
+4    {
+5        int c =0;
+6        int preSum =0;
+7        HashMap<Integer,Integer> map = new HashMap<>();
+8        map.put(0,1);
+9        for(int i =0;i<nums.length;i++)
+10        {
+11            preSum += nums[i];
+12            int remove = preSum - k;
+13            if(map.containsKey(remove))
+14            {
+15                c += map.get(remove);
+16             
+17            }
+18               map.put(preSum,map.getOrDefault(preSum,0)+1);
+19        }
+20
+21        return c;
+22    }
+23}
