03dd39d899c6c3f45d2a330e0878006582924e88
shreemathi-1 <shreemathi005@gmail.com>
2026-05-25 16:22:08 +0530

    File updated at 5/25/2026, 4:22:08 PM

--- 1046-LastStoneWeight/1046-LastStoneWeight.java
+++ 1046-LastStoneWeight/1046-LastStoneWeight.java
@@ -1,15 +1,21 @@
-// Last updated: 5/25/2026, 4:09:21 PM
+// Last updated: 5/25/2026, 4:22:08 PM
 1class Solution {
-2    public int findKthLargest(int[] nums, int k) {
-3        PriorityQueue<Integer> pq = new PriorityQueue<>();
-4        for(int num : nums)
-5        {
-6              pq.offer(num);
-7            if(pq.size() > k)
-8            {
-9                pq.poll();
-10            } 
-11        }
-12        return pq.poll();
-13    }
-14}
+2    public int[] deckRevealedIncreasing(int[] deck) {
+3        Arrays.sort(deck);
+4        int n = deck.length;
+5        Queue<Integer>  q = new LinkedList<>();
+6        int[] ans = new int[n];
+7        for(int i =0;i<n;i++)
+8        {
+9            q.offer(i);
+10        }
+11        for(int card : deck)
+12        {
+13           int index= q.poll();
+14           ans[index] = card;
+15           if(!q.isEmpty())
+16                q.offer(q.poll());
+17        }
+18        return ans;
+19    }
+20}
