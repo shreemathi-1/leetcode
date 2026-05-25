d1d7fed731d849ff063ae9b39729ab4205985a47
shreemathi-1 <shreemathi005@gmail.com>
2026-05-25 21:34:12 +0530

    File updated at 5/25/2026, 9:34:12 PM

--- 328-OddEvenLinkedList/328-OddEvenLinkedList.java
+++ 328-OddEvenLinkedList/328-OddEvenLinkedList.java
@@ -1,36 +1,15 @@
-// Last updated: 5/25/2026, 9:26:36 PM
-1class Solution {
-2    public int searchInsert(int[] nums, int target) 
-3    {
-4        int n = nums.length;
-5        int ans =0;
-6        int l =0;
-7        int r =n-1;
-8        int found = 0;
-9        int mid =0;
-10        while(l <= r)
-11        {
-12             mid = l + (r-l) /2;
-13            if(nums[mid] == target)
-14            {
-15                
-16                found =1;
-17                break;
-18                
-19            }
-20            else if(nums[mid] < target)
-21            {
-22                l = mid+1;
-23            }
-24            else
-25            {
-26                r = mid -1;
-27            }
-28        }
-29        if(found ==1)
-30        return mid;
-31        else
-32        return l;
-33       
-34    }
-35}
+// Last updated: 5/25/2026, 9:34:12 PM
+1public class Solution extends GuessGame {
+2    public int guessNumber(int n) {
+3        if (n == 1) return 1;
+4        int low = 1, high = n;
+5        while (low <= high){
+6            int mid = low + (high - low) / 2;
+7            int check = guess(mid);
+8            if (check == 0) return mid;
+9            else if (check == 1) low = mid + 1;
+10            else high = mid - 1;
+11        }
+12        return -1;
+13    }
+14}
