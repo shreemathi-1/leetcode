908432f63ccdf78928f252e79537891e9a835256
shreemathi-1 <shreemathi005@gmail.com>
2026-05-25 21:02:20 +0530

    File updated at 5/25/2026, 9:02:19 PM

--- 328-OddEvenLinkedList/328-OddEvenLinkedList.java
+++ 328-OddEvenLinkedList/328-OddEvenLinkedList.java
@@ -1,20 +1,21 @@
-// Last updated: 5/25/2026, 9:01:26 PM
-1class Solution 
-2{
-3    public int[] dailyTemperatures(int[] temperatures) 
-4    {
-5        int n = temperatures.length;
-6        Stack<Integer> st = new Stack<>();
-7        int[] temp = new int[n];
-8        for(int i =0;i<n;i++)
-9        {
-10            while(!st.isEmpty() && temperatures[i] > temperatures[st.peek()])
-11            {
-12                int ind = st.pop();
-13                temp[ind] = i - ind;
-14            }
-15            st.push(i);
+// Last updated: 5/25/2026, 9:02:19 PM
+1class Solution {
+2    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+3        int[] nextGreater = new int[10001];
+4        Stack<Integer> stack = new Stack<>();
+5
+6        for (int i = nums2.length - 1; i >= 0; i--) {
+7            while (!stack.isEmpty() && stack.peek() <= nums2[i]) {
+8                stack.pop();
+9            }
+10            nextGreater[nums2[i]] = stack.isEmpty() ? -1 : stack.peek();
+11            stack.push(nums2[i]);
+12        }
+13
+14        for (int i = 0; i < nums1.length; i++) {
+15            nums1[i] = nextGreater[nums1[i]];
 16        }
-17        return temp;
-18    }
-19}
+17
+18        return nums1;
+19    }
+20}
