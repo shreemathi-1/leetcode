795cbfb910e53a8bc503ce099d1544ea92fcd33a
shreemathi-1 <shreemathi005@gmail.com>
2026-05-25 21:40:51 +0530

    File updated at 5/25/2026, 9:40:50 PM

--- 328-OddEvenLinkedList/328-OddEvenLinkedList.java
+++ 328-OddEvenLinkedList/328-OddEvenLinkedList.java
@@ -1,25 +1,23 @@
-// Last updated: 5/25/2026, 9:38:41 PM
-1class Solution 
-2{
-3    public int findPeakElement(int[] nums) 
-4    {
-5        int n = nums.length;
-6        int l =0;
-7        int r = n-1;
-8        int mid =0;
-9        while(l<r)
-10        {  
-11            mid = l+ (r-l) /2;             
-12            if(nums[mid] > nums[mid+1])
-13            {
-14                r = mid ;
-15            }
-16            else
-17            {
-18                l = mid +1;
-19            }
-20
-21        }
-22        return l;
-23    }
-24}
+// Last updated: 5/25/2026, 9:40:50 PM
+1class Solution {
+2    public int search(int[] nums, int target) {
+3        int low = 0, high = nums.length - 1;
+4        while (low <= high) {
+5            int mid = (low + high) / 2;
+6            if (nums[mid] == target) 
+7                return mid;
+8            if (nums[low] <= nums[mid]) {
+9                if (nums[low] <= target && target < nums[mid]) 
+10                    high = mid - 1;
+11                 else 
+12                    low = mid + 1;
+13            } else {
+14                if (nums[mid] < target && target <= nums[high]) 
+15                    low = mid + 1;
+16                 else 
+17                    high = mid - 1;  
+18            }
+19        }
+20        return -1;
+21    }
+22}
