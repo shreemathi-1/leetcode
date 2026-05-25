74a0e1bf622026b5512a5d0c1942b74b3ed26ea8
shreemathi-1 <shreemathi005@gmail.com>
2026-05-25 16:09:21 +0530

    File updated at 5/25/2026, 4:09:21 PM

--- 1046-LastStoneWeight/1046-LastStoneWeight.java
+++ 1046-LastStoneWeight/1046-LastStoneWeight.java
@@ -1,20 +1,15 @@
-// Last updated: 5/25/2026, 4:03:09 PM
+// Last updated: 5/25/2026, 4:09:21 PM
 1class Solution {
-2    public int lastStoneWeight(int[] stones) {
-3        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
-4        for(int stone : stones)
+2    public int findKthLargest(int[] nums, int k) {
+3        PriorityQueue<Integer> pq = new PriorityQueue<>();
+4        for(int num : nums)
 5        {
-6            pq.offer(stone);
-7        }
-8        while(pq.size() > 1)
-9        {
-10            int y = pq.poll();
-11            int x = pq.poll();
-12            if(x != y)
-13            {
-14                pq.offer(y-x);
-15            }
-16        }
-17        return pq.size()==1 ? pq.peek() : 0;
-18    }
-19}
+6              pq.offer(num);
+7            if(pq.size() > k)
+8            {
+9                pq.poll();
+10            } 
+11        }
+12        return pq.poll();
+13    }
+14}
