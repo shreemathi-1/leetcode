4a6b3adccf11a860134d975e10a59b6882735a39
shreemathi-1 <shreemathi005@gmail.com>
2026-05-25 21:18:53 +0530

    File updated at 5/25/2026, 9:18:52 PM

--- 328-OddEvenLinkedList/328-OddEvenLinkedList.java
+++ 328-OddEvenLinkedList/328-OddEvenLinkedList.java
@@ -1,33 +1,30 @@
-// Last updated: 5/25/2026, 9:18:44 PM
-1import java.util.ArrayList;
-2import java.util.List;
-3
-4class Solution {
-5    public void backtrack(int[] nums, int start, List<List<Integer>> result) {
-6        if (start == nums.length) {
-7            List<Integer> current = new ArrayList<>();
-8            for (int num : nums) {
-9                current.add(num);
-10            }
-11            result.add(current);
-12            return;
-13        }
-14        for (int i = start; i < nums.length; i++) {
-15            swap(nums, start, i);
-16            backtrack(nums, start + 1, result);
-17            swap(nums, start, i); // backtrack
-18        }
-19    }
-20
-21    private void swap(int[] nums, int i, int j) {
-22        int temp = nums[i];
-23        nums[i] = nums[j];
-24        nums[j] = temp;
-25    }
-26
-27    public List<List<Integer>> permute(int[] nums) {
-28        List<List<Integer>> result = new ArrayList<>();
-29        backtrack(nums, 0, result);
-30        return result;
-31    }
-32}
+// Last updated: 5/25/2026, 9:18:52 PM
+1class Solution {
+2    public void backtrack(int[] nums, int start, List<List<Integer>> result) {
+3        if (start == nums.length) {
+4            List<Integer> current = new ArrayList<>();
+5            for (int num : nums) {
+6                current.add(num);
+7            }
+8            result.add(current);
+9            return;
+10        }
+11        for (int i = start; i < nums.length; i++) {
+12            swap(nums, start, i);
+13            backtrack(nums, start + 1, result);
+14            swap(nums, start, i); // backtrack
+15        }
+16    }
+17
+18    private void swap(int[] nums, int i, int j) {
+19        int temp = nums[i];
+20        nums[i] = nums[j];
+21        nums[j] = temp;
+22    }
+23
+24    public List<List<Integer>> permute(int[] nums) {
+25        List<List<Integer>> result = new ArrayList<>();
+26        backtrack(nums, 0, result);
+27        return result;
+28    }
+29}
