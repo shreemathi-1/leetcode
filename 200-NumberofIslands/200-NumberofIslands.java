b99242cf82a741adccfc7f40c3c9576660baf5ab
shreemathi-1 <shreemathi005@gmail.com>
2026-05-30 15:01:16 +0530

    File updated at 5/30/2026, 3:01:16 PM

--- 200-NumberofIslands/200-NumberofIslands.java
+++ 200-NumberofIslands/200-NumberofIslands.java
@@ -1,32 +1,6 @@
-// Last updated: 5/30/2026, 2:39:18 PM
+// Last updated: 5/30/2026, 3:01:16 PM
 1class Solution {
-2    public int numIslands(char[][] grid) {
-3        int count =0;
-4        int r= grid.length;
-5        int c = grid[0].length;
-6        for(int i =0;i<r;i++)
-7        {
-8            for(int j =0;j<c;j++)
-9            {
-10                if(grid[i][j]=='1')
-11                {
-12                     count++;
-13                    dfs(grid,i,j);     
-14                }
-15            }
-16        }
-17        return count;
-18    }
-19    public void dfs(char[][] grid,int i,int j)
-20    {
-21        if(i<0 || j<0 || i>=grid.length || j >= grid[0].length || grid[i][j] == '0')
-22        {
-23            return;
-24        }
-25        grid[i][j] ='0';
-26        dfs(grid,i+1,j);
-27        dfs(grid,i-1,j);
-28        dfs(grid,i,j+1);
-29        dfs(grid,i,j-1);
-30    }
-31}
+2    public int findCenter(int[][] edges) {
+3        return ((edges[0][0]==edges[1][0]) || (edges[0][0]==edges[1][1]))? edges[0][0] : edges[0][1]; 
+4    }
+5}
