2264c2e6fb9ebbd8f9e3b91505012a4c6b566449
shreemathi-1 <shreemathi005@gmail.com>
2026-05-30 19:08:06 +0530

    File updated at 5/30/2026, 7:08:06 PM

--- 442-FindAllDuplicatesinanArray/442-FindAllDuplicatesinanArray.java
+++ 442-FindAllDuplicatesinanArray/442-FindAllDuplicatesinanArray.java
@@ -1,24 +1,21 @@
-// Last updated: 5/30/2026, 6:48:49 PM
+// Last updated: 5/30/2026, 7:08:06 PM
 1class Solution 
 2{
 3    public List<Integer> findDuplicates(int[] nums) 
 4    {
-5        ArrayList<Integer> l = new ArrayList<>();
-6        int n = nums.length;
-7        HashSet<Integer> set = new HashSet<>();
-8        if(n ==0 || n==1)
-9        {
-10            return l;
-11        }
-12        
-13        for(int i=0;i<n;i++)
-14        {
-15            if(set.contains(nums[i]))
-16            {
-17                l.add(nums[i]);
-18            }
-19            set.add(nums[i]);
-20        }
-21        return l;
-22    }
-23}
+5       ArrayList<Integer> res = new ArrayList<>();
+6       int[] freq = new int[nums.length+1];
+7       for(int num : nums)
+8       {
+9        freq[num]++;
+10       }
+11       for(int i =1;i<freq.length;i++)
+12       {
+13            if(freq[i] == 2)
+14            {
+15                res.add(i);
+16            }
+17       }
+18       return res;
+19    }
+20}
