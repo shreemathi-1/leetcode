76e5cd03038c47d67925fa7c1f994db761873a5a
shreemathi-1 <shreemathi005@gmail.com>
2026-06-01 11:27:06 +0530

    File updated at 6/1/2026, 11:27:05 AM

--- 70-ClimbingStairs/70-ClimbingStairs.java
+++ 70-ClimbingStairs/70-ClimbingStairs.java
@@ -1,16 +1,14 @@
-// Last updated: 2/13/2026, 3:17:15 PM
-class Solution 
-{
-    public int climbStairs(int n) 
-    {
-        int[] dp = new int[n+1];
-        dp[0]=1;
-        dp[1] =1;
-        for(int i =2;i<n+1;i++)
-        {
-            dp[i]= dp[i-1] + dp[i-2];
-        }
-      
-        return dp[n];
-    }
-}
+// Last updated: 6/1/2026, 11:27:05 AM
+1class Solution {
+2    public int climbStairs(int n) {
+3        if(n<=2) return n;
+4        int dp[] = new int[n+1];        
+5        dp[1] = 1;
+6        dp[2] = 2;
+7        for(int i =3;i<=n;i++)
+8        {
+9            dp[i] = dp[i-1] + dp[i-2];
+10        }
+11        return dp[n];
+12    }
+13}
