78e75eeb50b87f5986f7a972cc0f27b284028661
shreemathi-1 <shreemathi005@gmail.com>
2026-06-01 20:32:35 +0530

    File updated at 6/1/2026, 8:32:35 PM

--- 518-CoinChangeII/518-CoinChangeII.java
+++ 518-CoinChangeII/518-CoinChangeII.java
@@ -1,30 +1,17 @@
-// Last updated: 6/1/2026, 8:18:30 PM
+// Last updated: 6/1/2026, 8:32:35 PM
 1class Solution {
-2    public int change(int amount, int[] coins) {
-3        int[] dp = new int[amount+1];
-4        dp[0] = 1;
-5        for(int coin : coins)
-6        {
-7            for(int i=coin;i<=amount;i++)
-8            {
-9                dp[i] += dp[i - coin];
-10            }
-11        }
-12        return dp[amount];
-13    }
-14}
-15
-16// public class Solution {
-17//     public int change(int amount, int[] coins) {
-18//         int[] dp = new int[amount + 1];
-19//         dp[0] = 1;
-20        
-21//         for (int coin : coins) {
-22//             for (int j = coin; j <= amount; j++) {
-23//                 dp[j] += dp[j - coin];
-24//             }
-25//         }
-26        
-27//         return dp[amount];
-28//     }
-29// }
+2    public int lengthOfLIS(int[] nums) {
+3        int len = nums.length, count = 1;
+4        int[] dp = new int[len];
+5        Arrays.fill(dp, 1);
+6        for(int i = 1; i<len; i++){
+7            for(int j = 0; j<i; j++){
+8                if(nums[j]<nums[i]){
+9                    dp[i] = Math.max(dp[i], dp[j]+1);
+10                }
+11            }
+12            count = Math.max(count, dp[i]);
+13        }
+14        return count;
+15    }
+16}
