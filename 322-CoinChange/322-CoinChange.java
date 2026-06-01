7f3b67dd46bbc484303ca914567b7931cc68a68a
shreemathi-1 <shreemathi005@gmail.com>
2026-06-01 15:01:27 +0530

    File updated at 6/1/2026, 3:01:27 PM

--- 322-CoinChange/322-CoinChange.java
+++ 322-CoinChange/322-CoinChange.java
@@ -1,23 +1,43 @@
-// Last updated: 6/1/2026, 3:00:15 PM
-1class Solution {
-2    public int coinChange(int[] coins, int amount) {
-3        int[] dp = new int[amount+1];
-4        Arrays.fill(dp,amount +1);
-5        dp[0] = 0;
-6        for(int coin : coins)
-7        {
-8            for(int i =coin;i<=amount;i++)
-9            {
-10                dp[i] = Math.min(dp[i],dp[i-coin]+1);
-11            }
-12        }
-13        if(dp[amount] > amount)
-14        {
-15            return -1;
-16        }
-17        else
-18        {
-19            return dp[amount];
-20        }
-21    }
-22}
+// Last updated: 6/1/2026, 3:01:27 PM
+class Solution {
+    int [] map;
+    
+    public int coinChange(int[] coins, int amount) {
+        if (amount == 0)
+            return 0;
+        
+        Arrays.sort(coins);
+        map = new int[amount + 1];
+        
+        for (int coin : coins){
+            if (coin <= amount)
+                map[coin] = 1;
+        }
+        
+        return findCoinChange(coins, amount);
+    }
+    
+    private int findCoinChange(int [] coins, int amount) {
+        if (map[amount] != 0)
+            return map[amount];
+        
+        int test; 
+        
+        int min = -1;
+        
+        for (int i = coins.length - 1; i >= 0; --i) {
+            if (min != -1 && coins[i]*min < amount)
+                break;
+            
+            if (amount > coins[i]) {
+                test = 1 + findCoinChange(coins, amount - coins[i]);
+                if (test != 0)
+                    min = min == -1 ? test : Math.min(test, min);
+            }
+        }
+        
+        map[amount] = min;
+        
+        return min;
+    }
+}
