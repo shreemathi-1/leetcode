7ed7efcf9faf8c2457fadbb4104952fe6790d0da
shreemathi-1 <shreemathi005@gmail.com>
2026-06-02 20:34:03 +0530

    File updated at 6/2/2026, 8:34:03 PM

--- 20-ValidParentheses/20-ValidParentheses.java
+++ 20-ValidParentheses/20-ValidParentheses.java
@@ -1,22 +1,28 @@
-// Last updated: 3/14/2026, 6:52:33 AM
-class Solution {
-    public boolean isValid(String s) {
-        Stack<Character> stack = new Stack<>();
-        Map<Character, Character> mapping = new HashMap<>();
-        mapping.put(')','(');
-        mapping.put('}','{'); 
-        mapping.put(']','[');  
-
-
-        for (char c : s.toCharArray()) {
-            if (mapping.containsValue(c)) {
-                stack.push(c);
-            } else if (mapping.containsKey(c)) {
-                if (stack.isEmpty() || mapping.get(c) != stack.pop()) {
-                    return false;
-                }
-            }
-        }
-        return stack.isEmpty();
-    }
-}
+// Last updated: 6/2/2026, 8:34:03 PM
+1class Solution {
+2    public boolean isValid(String s) {
+3        Stack<Character> st = new Stack<>();
+4        for(char ch : s.toCharArray())
+5        {
+6            if(ch == '(' || ch == '{' || ch == '[')
+7            {
+8                st.push(ch);
+9            }
+10            else
+11            {
+12                if(st.isEmpty())
+13                {
+14                    return false;
+15                }
+16                char top = st.pop();
+17                if(ch == ')' && top != '(' ||
+18                ch == ']' && top != '[' ||
+19                 ch == '}' && top != '{')
+20                 {
+21                    return false;
+22                 }
+23            }
+24        }
+25        return st.isEmpty();
+26    }
+27}
