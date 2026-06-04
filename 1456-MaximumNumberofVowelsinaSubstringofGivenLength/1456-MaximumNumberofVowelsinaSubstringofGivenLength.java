58e85426ad1c7a065600c09cfd1c2f2d7ae36d3e
shreemathi-1 <shreemathi005@gmail.com>
2026-06-04 21:42:57 +0530

    File updated at 6/4/2026, 9:42:57 PM

--- 1456-MaximumNumberofVowelsinaSubstringofGivenLength/1456-MaximumNumberofVowelsinaSubstringofGivenLength.java
+++ 1456-MaximumNumberofVowelsinaSubstringofGivenLength/1456-MaximumNumberofVowelsinaSubstringofGivenLength.java
@@ -1,37 +1,32 @@
-// Last updated: 3/9/2026, 12:17:59 PM
-1class Solution 
-2{
-3    static boolean isVowel(char ch)
-4 {
-5     if(ch =='a'|| ch =='e'|| ch =='i'|| ch =='o'|| ch =='u')
-6        return true;
-7     return false;
-8 }
-9    public int maxVowels(String s, int k) 
-10    {
-11        
-12   
-13        int max =0;
-14        int vow =0;
-15        for(int i =0;i<s.length();i++)
-16        {
-17            if(isVowel(s.charAt(i)))
-18            {
-19                vow++;
-20               
-21            }
-22            if(i>=k)
-23            {
-24                char ch = s.charAt(i-k);
-25                if(isVowel(ch))
-26                {
-27                    vow--;
-28                }
-29            }
-30            
-31             max = Math.max(max,vow);
-32            
-33        }
-34        return max;
-35    }
-36}
+// Last updated: 6/4/2026, 9:42:57 PM
+1class Solution {
+2    public boolean isVowel(char ch)
+3    {
+4        if(ch == 'a' ||ch == 'e' ||ch == 'i' ||ch == 'o' ||ch == 'u' )
+5        {
+6            return true;
+7        }
+8        return false;
+9    }
+10    public int maxVowels(String s, int k) {
+11        int vow =0,max =0;
+12        for(int i =0;i<s.length();i++)
+13        {
+14            if(isVowel(s.charAt(i)))
+15            {
+16                vow++;
+17            }
+18            if(i >=k)
+19            {
+20                if(isVowel(s.charAt(i-k)))
+21                {
+22                    vow-- ;
+23                }
+24            }
+25            max = Math.max(max,vow);
+26
+27        }
+28        
+29        return max;
+30    }
+31}
