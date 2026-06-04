ad051fdf3d173124518fe245f0c0b7f1b4fa7df6
shreemathi-1 <shreemathi005@gmail.com>
2026-06-04 22:20:46 +0530

    File updated at 6/4/2026, 10:20:45 PM

--- 3-LongestSubstringWithoutRepeatingCharacters/3-LongestSubstringWithoutRepeatingCharacters.java
+++ 3-LongestSubstringWithoutRepeatingCharacters/3-LongestSubstringWithoutRepeatingCharacters.java
@@ -1,25 +1,18 @@
-// Last updated: 3/30/2026, 9:35:08 PM
-1class Solution 
-2{
-3    public int lengthOfLongestSubstring(String s) 
-4    {
-5        LinkedHashSet set = new LinkedHashSet<>();
-6        int i =0,max =0,start =0;
-7        while(i<s.length())
-8        {
-9            if(!set.contains(s.charAt(i)))
-10            {
-11                
-12                set.add(s.charAt(i));
-13                max = Math.max(max,set.size());
-14                i++;
-15            }
-16            else
-17            {
-18                set.remove(s.charAt(start));
-19                start++;
-20            }
-21        }
-22        return max;
-23    }
-24}
+// Last updated: 6/4/2026, 10:20:45 PM
+1class Solution {
+2    public int lengthOfLongestSubstring(String s) {
+3        int ws=0,max =0;
+4        int[] freq = new int[256];
+5        for(int we =0;we < s.length();we++)
+6        {
+7            freq[s.charAt(we)]++;
+8            while(freq[s.charAt(we)] > 1)
+9            {
+10                freq[s.charAt(ws)]--;
+11                ws++;
+12            }
+13            max = Math.max(max,we - ws + 1);
+14        }
+15        return max;
+16    }
+17}
