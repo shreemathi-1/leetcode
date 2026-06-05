e70ff2f76ba4103df48e12ec17143f838f3616ba
shreemathi-1 <shreemathi005@gmail.com>
2026-06-05 12:16:31 +0530

    File updated at 6/5/2026, 12:16:31 PM

--- 328-OddEvenLinkedList/328-OddEvenLinkedList.java
+++ 328-OddEvenLinkedList/328-OddEvenLinkedList.java
@@ -1,4 +1,4 @@
-// Last updated: 6/5/2026, 11:44:50 AM
+// Last updated: 6/5/2026, 12:16:31 PM
 1/**
 2 * Definition for singly-linked list.
 3 * public class ListNode {
@@ -9,19 +9,37 @@
 8 *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
 9 * }
 10 */
-11class Solution {
-12    public ListNode oddEvenList(ListNode head) {
-13        if(head == null) return null;
-14        ListNode odd = head, even = head.next;
-15        ListNode evenHead = even;
-16        while(even != null && even.next != null)
+11class Solution 
+12{
+13    public void reorderList(ListNode head) 
+14    {
+15        ListNode slow = head , fast = head;
+16        while(fast != null && fast.next != null)
 17        {
-18            odd.next = even.next;
-19            odd = odd.next;
-20            even.next = odd.next;
-21            even = even.next;
-22        }
-23        odd.next = evenHead;
-24        return head;
-25    }
-26}
+18            slow = slow.next;
+19            fast = fast.next.next;
+20        }
+21
+22        ListNode cur = slow.next , next = null , prev = null;
+23        slow.next = null;
+24        while(cur != null)
+25        {
+26            next = cur.next ;
+27            cur.next = prev;
+28            prev = cur;
+29            cur = next;
+30        }
+31        ListNode secHead = prev;
+32        ListNode firHead = head;
+33        
+34        while(secHead != null )
+35        {
+36            ListNode t1 = firHead.next;
+37            ListNode t2 = secHead.next;
+38            firHead.next = secHead;
+39            secHead.next = t1;
+40            firHead = t1;
+41            secHead = t2;
+42        }
+43    }
+44}
