00062ea7b480e38ee5439e58735d7737e26b0835
shreemathi-1 <shreemathi005@gmail.com>
2026-06-06 15:43:14 +0530

    File updated at 6/6/2026, 3:43:14 PM

--- 319-BulbSwitcher/319-BulbSwitcher.java
+++ 319-BulbSwitcher/319-BulbSwitcher.java
@@ -1,26 +1,31 @@
-// Last updated: 6/6/2026, 2:58:03 PM
+// Last updated: 6/6/2026, 3:43:14 PM
 1class Solution {
-2    public int[][] transpose(int[][] matrix) {
-3        int[][] trans = new int[matrix[0].length][matrix.length];
-4        for(int i =0;i<matrix.length;i++)
-5        {
-6            for(int j =0;j<matrix[0].length;j++)
-7            {
-8                trans[j][i] = matrix[i][j];
-9            }
-10        }
-11return trans;
-12    }
-13}
-14
-15// OPTIMIZED CODE : works only for SQUARE MATRIX : 
-16//   for(int i =0;i<matrix.length;i++)
-17//         {
-18//             for(int j =i+1;j<matrix[0].length;j++)
-19//             {
-20//                 int temp = matrix[i][j];
-21//                 matrix[i][j] = matrix[j][i];
-22//                 matrix[j][i] = temp;
-23//             }
-24//         }
-25// return matrix;
+2    public int[] reverse(int[] row)
+3    {
+4        int l=0,r=row.length-1;
+5        while(l<r)
+6        {
+7           int temp = row[l];
+8           row[l] = row[r];
+9           row[r] = temp;
+10           l++;
+11           r--;
+12        }
+13        return row;
+14    }
+15    public void rotate(int[][] matrix) {
+16        for(int i =0;i<matrix.length;i++)
+17        {
+18            for(int j =i+1;j<matrix[0].length;j++)
+19            {
+20                int temp = matrix[i][j];
+21                matrix[i][j] = matrix[j][i];
+22                matrix[j][i] = temp;
+23            }
+24        }
+25          for(int i=0;i<matrix.length;i++)
+26            {
+27                reverse(matrix[i]);
+28            }
+29    }
+30}
