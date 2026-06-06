eaa7d27170b3f830aeacf44cebab9e3ada6e24c9
shreemathi-1 <shreemathi005@gmail.com>
2026-06-06 14:58:03 +0530

    File updated at 6/6/2026, 2:58:03 PM

--- 319-BulbSwitcher/319-BulbSwitcher.java
+++ 319-BulbSwitcher/319-BulbSwitcher.java
@@ -1,4 +1,4 @@
-// Last updated: 6/6/2026, 2:45:47 PM
+// Last updated: 6/6/2026, 2:58:03 PM
 1class Solution {
 2    public int[][] transpose(int[][] matrix) {
 3        int[][] trans = new int[matrix[0].length][matrix.length];
@@ -12,3 +12,15 @@
 11return trans;
 12    }
 13}
+14
+15// OPTIMIZED CODE : works only for SQUARE MATRIX : 
+16//   for(int i =0;i<matrix.length;i++)
+17//         {
+18//             for(int j =i+1;j<matrix[0].length;j++)
+19//             {
+20//                 int temp = matrix[i][j];
+21//                 matrix[i][j] = matrix[j][i];
+22//                 matrix[j][i] = temp;
+23//             }
+24//         }
+25// return matrix;
