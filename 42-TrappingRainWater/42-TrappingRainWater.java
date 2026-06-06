63c0ff43090e08b2b06467b12866ea150e7df366
shreemathi-1 <shreemathi005@gmail.com>
2026-06-06 12:25:06 +0530

    File updated at 6/6/2026, 12:25:06 PM

--- 42-TrappingRainWater/42-TrappingRainWater.java
+++ 42-TrappingRainWater/42-TrappingRainWater.java
@@ -1,21 +1,25 @@
-// Last updated: 3/2/2026, 8:46:02 PM
-class Solution {
-    public int trap(int[] ar) {
-        int[] leftMax = new int[ar.length];
-        int[] rightMax = new int[ar.length];
-        int maxWater = 0;
-        leftMax[0] = ar[0];
-        rightMax[ar.length - 1] = ar[ar.length-1];
-        for (int i = 1; i < ar.length; i++) {
-            leftMax[i] = Math.max(leftMax[i-1], ar[i]);
-        }
-        for (int i = ar.length - 2; i >= 0; i--) {
-            rightMax[i] = Math.max(rightMax[i+1],ar[i]);
-        }
-
-        for (int i = 1; i < ar.length-1; i++) {
-            maxWater += Math.min(leftMax[i], rightMax[i]) - ar[i];
-        }
-        return maxWater;
-    }
-}
+// Last updated: 6/6/2026, 12:25:06 PM
+1class Solution {
+2    public int trap(int[] height) {
+3        if(height == null || height.length <3)
+4        {
+5            return 0;
+6        }
+7        Stack<Integer> st = new Stack<>();
+8        st.push(height[height.length-1]);
+9        int leftMax= height[0];
+10        int water =0;
+11        for(int i = height.length -2 ;i>=2;i--)
+12        {
+13            st.push(Math.max(height[i],st.peek()));
+14        }
+15        for(int i =1; i< height.length-1;i++)
+16        {   
+17             int minHeight = Math.min(leftMax,st.peek());
+18             water += Math.max(0,minHeight - height[i]);
+19             st.pop();
+20             leftMax = Math.max(leftMax,height[i]);
+21        }   
+22        return water;
+23    }
+24}
