49bf86e57d6932abbe2e4d1b3dd38b86f576ba17
shreemathi-1 <shreemathi005@gmail.com>
2026-06-07 19:38:39 +0530

    File updated at 6/7/2026, 7:38:39 PM

--- 1004-MaxConsecutiveOnesIII/1004-MaxConsecutiveOnesIII.java
+++ 1004-MaxConsecutiveOnesIII/1004-MaxConsecutiveOnesIII.java
@@ -1,23 +1,24 @@
-// Last updated: 6/4/2026, 2:10:44 PM
+// Last updated: 6/7/2026, 7:38:39 PM
 1class Solution {
 2    public int longestOnes(int[] nums, int k) {
-3        int max =0,flip=0, ws =0;
-4        for(int we =0;we<nums.length;we++)
-5        {
-6            if(nums[we] ==0)
-7            {
-8                flip++;
-9            }
-10            while(flip > k)
-11            {
-12                if(nums[ws] ==0)
-13                {
-14                    flip--;
-15                }
-16                ws++;
-17            }
-18            max = Math.max(max,we-ws+1);
-19        }
-20    return max;
-21    }
-22}
+3        int flip =0 ,max =0;
+4        int ws =0;
+5        for(int we =0; we < nums.length ; we++)
+6        {
+7           if(nums[we] == 0)
+8           {
+9                flip++;
+10           }
+11           while(flip > k)
+12           {
+13            if(nums[ws] ==0)
+14            {
+15                flip--;
+16            }
+17            ws++;               
+18           }
+19           max = Math.max(max, we - ws + 1);
+20        }
+21        return max ;
+22    }
+23}
