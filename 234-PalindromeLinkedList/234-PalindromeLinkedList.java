9f0369d013c9b8a253c4dbc9ede52dd85f4222bd
shreemathi-1 <shreemathi005@gmail.com>
2026-06-08 12:44:24 +0530

    File updated at 6/8/2026, 12:44:24 PM

--- 234-PalindromeLinkedList/234-PalindromeLinkedList.java
+++ 234-PalindromeLinkedList/234-PalindromeLinkedList.java
@@ -1,4 +1,4 @@
-// Last updated: 5/6/2026, 11:08:57 AM
+// Last updated: 6/8/2026, 12:44:24 PM
 1/**
 2 * Definition for singly-linked list.
 3 * public class ListNode {
@@ -9,43 +9,42 @@
 8 *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
 9 * }
 10 */
-11class Solution 
-12{
-13    public static ListNode reverse(ListNode head)
-14    {
-15        ListNode prev = null,next = null;
-16        while(head != null)
-17        {
-18            next = head.next;
-19            head.next = prev;
-20            prev = head;
-21            head = next;
-22        }
-23        return prev;
-24    }
-25    public boolean isPalindrome(ListNode head) 
-26    {
-27        if(head == null || head.next == null)
+11class Solution {
+12    public ListNode reverse(ListNode head)
+13    {
+14        ListNode cur = head, prev = null , next = null;
+15        while(cur != null)
+16        {
+17            next = cur.next;
+18            cur.next = prev;
+19            prev = cur ;
+20            cur = next;
+21        }
+22        return prev;
+23    }
+24    public boolean isPalindrome(ListNode head) {
+25        if(head == null || head.next == null) return true;
+26        ListNode slow = head, fast = head;
+27        while(fast != null && fast.next != null)
 28        {
-29            return true;
-30        }
-31        ListNode fast = head,slow = head;
-32        while(fast != null && fast.next != null)
-33        {
-34            slow = slow.next;
-35            fast = fast.next.next;
-36        }
-37        ListNode fh = head;
-38        ListNode sh = reverse(slow);
-39        while(sh != null)
-40        {
-41            if(fh.val != sh.val)
-42            {
-43                return false;
-44            }
-45            fh = fh.next;
-46            sh= sh.next;
-47        }
-48        return true;
-49    }
-50}
+29            slow = slow.next;
+30            fast = fast.next.next;
+31        }
+32        ListNode mid= slow;
+33        
+34
+35        ListNode rHead = reverse(mid);
+36        ListNode rTemp = rHead;
+37        ListNode temp = head;
+38        while(rTemp != null )
+39        {
+40            if(temp.val != rTemp.val)
+41            {
+42                return false;
+43            }
+44            temp = temp.next;
+45            rTemp = rTemp.next;
+46        }
+47        return true;
+48    }
+49}
