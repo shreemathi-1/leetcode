fc3bd22f8610065c50ed238f9aa8e2b2116e8098
shreemathi-1 <shreemathi005@gmail.com>
2026-06-10 21:40:06 +0530

    File updated at 6/10/2026, 9:40:05 PM

--- 169-MajorityElement/169-MajorityElement.java
+++ 169-MajorityElement/169-MajorityElement.java
@@ -1,25 +1,27 @@
-// Last updated: 4/8/2026, 8:54:07 PM
-1class Solution 
-2{
-3    public int majorityElement(int[] nums) 
-4    {
-5        int maj = nums[0];
-6        int c =0;
-7        for(int i =0;i<nums.length;i++)
-8        {
-9            if(c==0 )
-10            {
-11                maj = nums[i];
-12            }
-13            if(nums[i] == maj)
-14            {
-15                c++;
-16            }
-17            else
-18            {
-19                c--;
-20            }
-21        }
-22        return maj;
-23    }
-24}
+// Last updated: 6/10/2026, 9:40:05 PM
+1class Solution {
+2    public int majorityElement(int[] nums) {
+3        int maj =0, c=0;
+4        for(int num : nums)
+5        {
+6            if(c==0)
+7            {
+8                maj = num;
+9            }
+10            if(num == maj)
+11                c++;
+12            else
+13                c--;    
+14        }
+15        int freq = 0;
+16        for(int num : nums)
+17        {
+18            if(num == maj)
+19                freq++;
+20        }
+21        if(freq > nums.length/2)
+22            return maj;
+23        else 
+24            return -1;
+25    }
+26}
