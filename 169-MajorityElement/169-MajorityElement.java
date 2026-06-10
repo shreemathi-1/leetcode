bfc811d26dab4e7017ee8dc472ed9f24fbd1d721
shreemathi-1 <shreemathi005@gmail.com>
2026-06-10 22:31:22 +0530

    File updated at 6/10/2026, 10:31:21 PM

--- 169-MajorityElement/169-MajorityElement.java
+++ 169-MajorityElement/169-MajorityElement.java
@@ -1,4 +1,4 @@
-// Last updated: 6/10/2026, 10:30:21 PM
+// Last updated: 6/10/2026, 10:31:21 PM
 1class Solution 
 2{
 3    public int diagonalSum(int[][] mat) 
@@ -7,13 +7,10 @@
 6        for(int i =0;i<mat.length;i++)
 7        {
 8            sum += mat[i][i];
-9        }
-10        for(int i =0;i<mat[0].length;i++)
-11        {
-12            sum += mat[i][mat.length - i - 1];
-13        }
-14           if(mat.length % 2 != 0)
-15                sum -= mat[mat.length/2][mat.length/2];
-16        return sum;
-17    }
-18}
+9            sum += mat[i][mat.length - i - 1];
+10        }
+11        if(mat.length % 2 != 0)
+12            sum -= mat[mat.length/2][mat.length/2];
+13        return sum;
+14    }
+15}
