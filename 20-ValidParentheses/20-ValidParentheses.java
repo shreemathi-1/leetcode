af8ec78129bf502462ec0a6d8d3bd1b6a6e1c631
shreemathi-1 <shreemathi005@gmail.com>
2026-06-10 18:12:28 +0530

    File updated at 6/10/2026, 6:12:28 PM

--- 20-ValidParentheses/20-ValidParentheses.java
+++ 20-ValidParentheses/20-ValidParentheses.java
@@ -1,38 +1,27 @@
-// Last updated: 6/2/2026, 9:36:43 PM
+// Last updated: 6/10/2026, 6:12:28 PM
 1class Solution {
-2    public int evalRPN(String[] tokens) {
-3        Stack<Integer> st = new Stack<>();
-4        int a, b;
-5        for(String val : tokens)
-6        {
-7           
-8            switch(val)
-9            {
-10                    case "+":
-11                      b =  st.pop();
-12                 a = st.pop();
-13                        st.push(a+b);
-14                        break;
-15                    case "-":
-16                      b =st.pop();
-17                 a = st.pop();
-18                    st.push(a-b);
-19                    break;
-20                    case "*":
-21                      b =  st.pop();
-22                 a = st.pop();
-23                    st.push(a*b);
-24                    break;
-25                    case "/":
-26                     b =  st.pop();
-27                a =  st.pop();
-28                    st.push(a/b);
-29                    break;    
-30                    default:
-31                    st.push(Integer.parseInt(val) );
-32                
-33            }
-34        }
-35        return st.pop();
-36    }
-37}
+2    public boolean isValid(String s) {
+3        Stack<Character> st = new Stack<>();
+4        for(char ch : s.toCharArray())
+5        {
+6            if(ch == '(' || ch == '{' || ch == '[')
+7            {
+8                st.push(ch);
+9            }
+10            else
+11            {
+12                if(st.isEmpty())
+13                {
+14                    return false;
+15                }
+16                if(ch == ')' && st.pop() != '(' 
+17                || ch == '}' && st.pop() != '{'
+18                || ch == ']' && st.pop() != '[' ) 
+19                {
+20                    return false;
+21                }
+22            }
+23        }
+24        return st.isEmpty();
+25    }
+26}
