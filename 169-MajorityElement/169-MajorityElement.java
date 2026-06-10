4da8c37c7ba72ebf2b529dbe7133cc7951d38372
shreemathi-1 <shreemathi005@gmail.com>
2026-06-10 22:30:22 +0530

    File updated at 6/10/2026, 10:30:21 PM

--- 169-MajorityElement/169-MajorityElement.java
+++ 169-MajorityElement/169-MajorityElement.java
@@ -1,27 +1,19 @@
-// Last updated: 6/10/2026, 9:40:05 PM
-1class Solution {
-2    public int majorityElement(int[] nums) {
-3        int maj =0, c=0;
-4        for(int num : nums)
-5        {
-6            if(c==0)
-7            {
-8                maj = num;
-9            }
-10            if(num == maj)
-11                c++;
-12            else
-13                c--;    
-14        }
-15        int freq = 0;
-16        for(int num : nums)
-17        {
-18            if(num == maj)
-19                freq++;
-20        }
-21        if(freq > nums.length/2)
-22            return maj;
-23        else 
-24            return -1;
-25    }
-26}
+// Last updated: 6/10/2026, 10:30:21 PM
+1class Solution 
+2{
+3    public int diagonalSum(int[][] mat) 
+4    {
+5        int sum =0;
+6        for(int i =0;i<mat.length;i++)
+7        {
+8            sum += mat[i][i];
+9        }
+10        for(int i =0;i<mat[0].length;i++)
+11        {
+12            sum += mat[i][mat.length - i - 1];
+13        }
+14           if(mat.length % 2 != 0)
+15                sum -= mat[mat.length/2][mat.length/2];
+16        return sum;
+17    }
+18}
