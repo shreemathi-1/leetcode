bb6805da521a377e018f40e4789cbdf6c5c2fa22
shreemathi-1 <shreemathi005@gmail.com>
2026-06-10 22:58:45 +0530

    File updated at 6/10/2026, 10:58:45 PM

--- 169-MajorityElement/169-MajorityElement.java
+++ 169-MajorityElement/169-MajorityElement.java
@@ -1,16 +1,16 @@
-// Last updated: 6/10/2026, 10:31:21 PM
-1class Solution 
-2{
-3    public int diagonalSum(int[][] mat) 
-4    {
-5        int sum =0;
-6        for(int i =0;i<mat.length;i++)
-7        {
-8            sum += mat[i][i];
-9            sum += mat[i][mat.length - i - 1];
-10        }
-11        if(mat.length % 2 != 0)
-12            sum -= mat[mat.length/2][mat.length/2];
-13        return sum;
+// Last updated: 6/10/2026, 10:58:45 PM
+1class Solution {
+2    public int[][] construct2DArray(int[] original, int m, int n) 
+3    {
+4        if(original.length != m*n)
+5        {
+6            return new int[][]{};
+7        }
+8        int mat[][] = new int[m][n];
+9        for(int i =0;i< original.length;i++)
+10        {
+11            mat[i/n][i%n] = original[i];
+12        }
+13        return mat;
 14    }
 15}
