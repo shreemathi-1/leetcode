607c4f86c2ce29f4117361c009a3634d0533aed6
shreemathi-1 <shreemathi005@gmail.com>
2026-07-02 22:44:53 +0530

    File updated at 7/2/2026, 10:44:52 PM

--- 930-BinarySubarraysWithSum/930-BinarySubarraysWithSum.java
+++ 930-BinarySubarraysWithSum/930-BinarySubarraysWithSum.java
@@ -1,4 +1,4 @@
-// Last updated: 7/2/2026, 10:43:58 PM
+// Last updated: 7/2/2026, 10:44:52 PM
 1class Solution 
 2{
 3    public int numSubarraysWithSum(int[] nums, int goal) 
@@ -6,20 +6,17 @@
 5        HashMap<Integer,Integer> map = new HashMap<>();
 6        int count = 0;
 7        int prefSum = 0,rem = 0;
-8        for(int i =0;i<nums.length;i++)
-9        {
-10            prefSum += nums[i];
-11            rem = prefSum - goal;
-12            if(prefSum == goal)
-13            {
-14                count++;
-15            }
-16            if(map.containsKey(rem))
-17            {
-18                count += map.get(rem);
-19            }
-20            map.put(prefSum , map.getOrDefault(prefSum ,0 )+1);
-21        }
-22        return count;
-23    }
-24}
+8        map.put(0,1);
+9        for(int i =0;i<nums.length;i++)
+10        {
+11            prefSum += nums[i];
+12            rem = prefSum - goal;
+13            if(map.containsKey(rem))
+14            {
+15                count += map.get(rem);
+16            }
+17            map.put(prefSum , map.getOrDefault(prefSum ,0 )+1);
+18        }
+19        return count;
+20    }
+21}
