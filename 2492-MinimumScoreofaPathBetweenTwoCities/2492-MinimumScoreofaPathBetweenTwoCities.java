987b79abd4f2db81c0805faad4f40599b65c57fb
shreemathi-1 <shreemathi005@gmail.com>
2026-07-04 22:29:49 +0530

    File updated at 7/4/2026, 10:29:49 PM

--- 2492-MinimumScoreofaPathBetweenTwoCities/2492-MinimumScoreofaPathBetweenTwoCities.java
+++ 2492-MinimumScoreofaPathBetweenTwoCities/2492-MinimumScoreofaPathBetweenTwoCities.java
@@ -1,40 +1,26 @@
-// Last updated: 7/4/2026, 10:26:13 PM
+// Last updated: 7/4/2026, 10:29:49 PM
 1class Solution {
-2    int[] root;
-3
-4    private int find(int x) {
-5        if (root[x] != x) {
-6            root[x] = find(root[x]);
-7        }
-8        return root[x];
-9    }
-10
-11    public int minScore(int n, int[][] roads) {
-12        root = new int[n + 1];
-13
-14        for (int i = 0; i <= n; i++) {
-15            root[i] = i;
-16        }
-17
-18        for (int[] road : roads) {
-19            int x = road[0];
-20            int y = road[1];
-21
-22            root[find(x)] = find(y);
-23        }
-24
-25        int res = 1_000_000_000;
-26        int g1 = find(1);
-27
-28        for (int[] road : roads) {
-29            int x = road[0];
-30            int d = road[2];
-31
-32            if (find(x) == g1) {
-33                res = Math.min(res, d);
-34            }
-35        }
-36
-37        return res;
-38    }
-39}
+2    public int minScore(int n, int[][] roads) {
+3        List<List<int[]>>list=new ArrayList<>();
+4        for(int i=0;i<=n;i++)list.add(new ArrayList<>());
+5        for(int[]row:roads){
+6            list.get(row[0]).add(new int[]{row[1],row[2]});
+7            list.get(row[1]).add(new int[]{row[0],row[2]});
+8        }
+9        int ans=Integer.MAX_VALUE;
+10        boolean[]visited=new boolean[n+1];
+11        Queue<Integer>queue=new LinkedList<>();
+12        queue.offer(1);
+13        while(!queue.isEmpty()){
+14            int curNode=queue.poll();
+15            for(int[]node:list.get(curNode)){
+16                ans=Math.min(ans,node[1]);
+17                if(!visited[node[0]]){
+18                    visited[node[0]]=true;
+19                    queue.offer(node[0]);
+20                }
+21            }
+22        }
+23        return ans;
+24    }
+25}
