dc2446a02cf18a8008b48823f84d4975b4bbedaf
shreemathi-1 <shreemathi005@gmail.com>
2026-07-04 22:28:22 +0530

    File updated at 7/4/2026, 10:28:22 PM

--- 2095-DeletetheMiddleNodeofaLinkedList/2095-DeletetheMiddleNodeofaLinkedList.java
+++ 2095-DeletetheMiddleNodeofaLinkedList/2095-DeletetheMiddleNodeofaLinkedList.java
@@ -1,15 +1,34 @@
-// Last updated: 7/4/2026, 10:26:42 PM
+// Last updated: 7/4/2026, 10:28:22 PM
 1class Solution {
-2    public ListNode deleteMiddle(ListNode head) {
-3        if(head == null || head.next == null) return null;
-4        ListNode prev=null,slow=head,fast=head;
-5        while(fast != null && fast.next != null){
-6            prev = slow;
-7            slow = slow.next;
-8            fast = fast.next.next;
-9        }
-10        if(prev == null) return null;
-11        prev.next = slow.next;
-12        return head;
-13    }
-14}
+2    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+3        Set<String> wordSet = new HashSet<>(wordList);
+4        if (!wordSet.contains(endWord)) return 0;
+5
+6        Queue<String> queue = new LinkedList<>();
+7        queue.offer(beginWord);
+8        int steps = 1;
+9
+10        while (!queue.isEmpty()) {
+11            int size = queue.size();
+12            while (size-- > 0) {
+13                String word = queue.poll();
+14                if (word.equals(endWord)) return steps;
+15
+16                for (int i = 0; i < word.length(); i++) {
+17                    for (char c = 'a'; c <= 'z'; c++) {
+18                        char[] chars = word.toCharArray();
+19                        chars[i] = c;
+20                        String newWord = new String(chars);
+21
+22                        if (wordSet.contains(newWord)) {
+23                            queue.offer(newWord);
+24                            wordSet.remove(newWord);
+25                        }
+26                    }
+27                }
+28            }
+29            steps++;
+30        }
+31        return 0;
+32    }
+33}
