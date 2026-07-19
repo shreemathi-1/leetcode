3f7e1a442876a9cbacf38596943793e46471380f
shreemathi-1 <shreemathi005@gmail.com>
2026-07-19 09:06:50 +0530

    File updated at 7/19/2026, 9:06:50 AM

--- 1081-SmallestSubsequenceofDistinctCharacters/1081-SmallestSubsequenceofDistinctCharacters.java
+++ 1081-SmallestSubsequenceofDistinctCharacters/1081-SmallestSubsequenceofDistinctCharacters.java
@@ -1,11 +1,15 @@
-// Last updated: 7/19/2026, 8:56:14 AM
+// Last updated: 7/19/2026, 9:06:50 AM
 1class Solution {
-2    public double angleClock(int hour, int minutes) {
-3        double theta =  Math.abs((30 * hour ) - (5.5*minutes));
-4        if(theta > 180)
-5        {
-6            theta = 360 - theta;
-7        }
-8        return theta;
-9    }
-10}
+2    public int largestAltitude(int[] gain) {
+3        int max = 0;
+4           int sum =0;
+5        for(int i =0;i<gain.length;i++)
+6        {
+7         
+8                sum += gain[i];
+9                max = Math.max(max,sum);
+10            
+11        }
+12        return max;
+13    }
+14}
