187f1e8f9b2cc4593be1392eebe3ee01e1cc5294
shreemathi-1 <shreemathi005@gmail.com>
2026-07-19 09:30:38 +0530

    File updated at 7/19/2026, 9:30:38 AM

--- 1081-SmallestSubsequenceofDistinctCharacters/1081-SmallestSubsequenceofDistinctCharacters.java
+++ 1081-SmallestSubsequenceofDistinctCharacters/1081-SmallestSubsequenceofDistinctCharacters.java
@@ -1,27 +1,25 @@
-// Last updated: 7/19/2026, 9:24:03 AM
+// Last updated: 7/19/2026, 9:30:38 AM
 1class Solution {
 2    public int maxNumberOfBalloons(String text) {
-3        HashMap<Character,Integer> map = new HashMap<>();
-4        String b = "balloon";
-5        for(char ch : b.toCharArray())
-6        {
-7            map.put(ch , 0);
-8        }
-9        for(char ch : text.toCharArray())
-10        {
-11            if(b.contains(""+ch))
-12            {
-13                map.put(ch , map.get(ch)+1);
-14            }
-15        }
-16        map.put('l',map.get('l')/2);
-17        map.put('o',map.get('o')/2);
-18        int min=Integer.MAX_VALUE;
-19        for(char ch : map.keySet())
-20        {
-21            min = Math.min(min, map.get(ch));
-22        }
-23       
-24        return min;
-25    }
-26}
+3       int b=0;
+4       int a=0;
+5       int l=0;
+6       int o=0;
+7       int n=0;
+8       for(char c:text.toCharArray()){
+9        if(c=='b'){
+10            b++;
+11        }else if(c=='a'){
+12            a++;
+13        }else if(c=='l'){
+14            l++;
+15        }else if(c=='o'){
+16            o++;
+17        }else if(c=='n'){
+18            n++;
+19        }
+20       }
+21       int res = Math.min(b, Math.min(a , Math.min(l/2,Math.min(o/2,n))));
+22       return res; 
+23    }
+24}
