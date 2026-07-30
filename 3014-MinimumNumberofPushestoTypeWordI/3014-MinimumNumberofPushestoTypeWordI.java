b6d857a9d7351e80dc40e72b960a4ed481912155
shreemathi-1 <shreemathi005@gmail.com>
2026-07-30 21:26:49 +0530

    File updated at 7/30/2026, 9:26:49 PM

--- 3014-MinimumNumberofPushestoTypeWordI/3014-MinimumNumberofPushestoTypeWordI.java
+++ 3014-MinimumNumberofPushestoTypeWordI/3014-MinimumNumberofPushestoTypeWordI.java
@@ -1,14 +1,11 @@
-// Last updated: 7/30/2026, 9:15:10 PM
+// Last updated: 7/30/2026, 9:26:49 PM
 1class Solution {
 2    public int minimumPushes(String word) {
-3
-4        int pushes = 0;
-5
-6        for (int i = 0; i < word.length(); i++) {
-7
-8            pushes += (i / 8) + 1;
-9        }
-10
-11        return pushes;
-12    }
-13}
+3        int pushes = 0;
+4        for(int i =0;i<word.length();i++)
+5        {
+6            pushes += (i / 8) + 1;
+7        }
+8        return pushes;
+9    }
+10}
