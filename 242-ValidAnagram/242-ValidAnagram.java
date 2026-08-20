2bd4cc17aa2709f62a3fcbc541b83e3f191fcc13
shreemathi-1 <shreemathi005@gmail.com>
2026-08-20 16:23:53 +0530

    File updated at 8/20/2026, 4:23:52 PM

--- 242-ValidAnagram/242-ValidAnagram.java
+++ 242-ValidAnagram/242-ValidAnagram.java
@@ -1,20 +1,23 @@
-// Last updated: 2/13/2026, 3:16:25 PM
-class Solution 
-{
-    public boolean isAnagram(String s, String t) 
-    {
-        if(s.length() != t.length())
-        {
-            return false;
-        }
-        int[] freq = new int[26];
-        for(char ch : s.toCharArray())  freq[ch-'a']++;
-        for(char ch : t.toCharArray())  freq[ch-'a']--;
-        for(int val : freq)
-        {
-            if(val !=0)
-            return false;
-        }
-        return true;
-    }
-}
+// Last updated: 8/20/2026, 4:23:52 PM
+1class Solution {
+2    public boolean isAnagram(String s, String t) {
+3        if(s.length() != t.length())
+4        {
+5            return false;
+6        }
+7        int[] freq = new int[26];
+8        for(int i =0;i<s.length();i++)
+9        {
+10            freq[s.charAt(i) - 'a']++;
+11            freq[t.charAt(i) - 'a']--;
+12        }
+13        for(int i =0;i<freq.length;i++)
+14        {
+15            if(freq[i] != 0)
+16            {
+17                return false;
+18            }
+19        }
+20        return true;
+21    }
+22}
