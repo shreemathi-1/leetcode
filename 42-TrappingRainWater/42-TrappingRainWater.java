376beaf92636ac581a525ec6082e7680ddefc5b0
shreemathi-1 <shreemathi005@gmail.com>
2026-08-21 15:00:17 +0530

    File updated at 8/21/2026, 3:00:16 PM

--- 42-TrappingRainWater/42-TrappingRainWater.java
+++ 42-TrappingRainWater/42-TrappingRainWater.java
@@ -1,35 +1,36 @@
-// Last updated: 8/3/2026, 4:21:04 PM
+// Last updated: 8/21/2026, 3:00:16 PM
 1class Solution {
 2    public int trap(int[] height) {
-3        int left =0 , right = height.length-1;
-4        int lMax = 0 , rMax = 0 , water =0 ;
-5        while(left < right)
-6        {
-7            if(height[left] < height[right])
-8            {
-9                if(height[left] > lMax)
-10                {
-11                    lMax = height[left];
-12                }
-13                else
-14                {
-15                    water += lMax - height[left];
-16                }
-17                left++;
-18            }
-19            else
-20            {
-21                if(height[right] > rMax)
-22                {
-23                    rMax = height[right];
-24                }
-25                else
-26                {
-27                    water += rMax - height[right];
-28                }
-29                right--;
-30            }
-31        }
-32        return water;
-33    }
-34}
+3        int water = 0;
+4        int left = 0, right = height.length-1;
+5        int leftMax = 0, rightMax = 0;
+6        while(left < right)
+7        {
+8            if(height[left] < height[right])
+9            {
+10                if(height[left] > leftMax)
+11                {
+12                    leftMax = height[left];
+13                }
+14                else
+15                {
+16                    water += leftMax - height[left];
+17                }
+18                left++;
+19            }
+20            else
+21            {
+22                if(height[right] > rightMax)
+23                {
+24                    rightMax = height[right];
+25                }
+26                else
+27                {
+28                    water += rightMax - height[right];
+29                }
+30                right--;
+31            }
+32        }
+33        return water;
+34    }
+35}
